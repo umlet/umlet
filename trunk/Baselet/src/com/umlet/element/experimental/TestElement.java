dd6cb26d8aa052a97340b8f6f4e932d6d26e0217
AFDiaX <AFDiaX@79637776-beb9-4c1a-dc75-bc3ee3110a07>
2012-07-14 14:05:20 +0000

    NewGridElement: wird nicht laufend neu initialisiert

--- trunk/Baselet/src/com/umlet/element/experimental/TestElement.java
+++ trunk/Baselet/src/com/umlet/element/experimental/TestElement.java
@@ -11,6 +11,7 @@ public class TestElement extends NewGridElement {
 	@Override
 	public void updateModelFromText() {
 		drawables.clear();
+		drawer.setSize(getSize());
 		drawables.add(new Rectangle(0, 0, getRealSize().width-1, getRealSize().height-1));
 		drawables.addAll(Facets.getClassDrawables(this.getPanelAttributes(), drawer.textHeight()+2, getRealSize().width-1));
 	}
