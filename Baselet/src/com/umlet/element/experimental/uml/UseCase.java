dfa9ed88a3fec774dc0621409647ef5df6afa18a
AFDiaX <AFDiaX@79637776-beb9-4c1a-dc75-bc3ee3110a07>
2012-07-23 19:08:36 +0000

    NewGridElement: Background of element is in light blue shade (to see the border eg for resizing), stickingborder is shown and handled correctly

--- Baselet/src/com/umlet/element/experimental/uml/UseCase.java
+++ Baselet/src/com/umlet/element/experimental/uml/UseCase.java
@@ -1,5 +1,6 @@
 package com.umlet.element.experimental.uml;
 
+import com.baselet.element.StickingPolygon;
 import com.umlet.element.experimental.Id;
 import com.umlet.element.experimental.NewGridElement;
 
@@ -14,6 +15,26 @@ public class UseCase extends NewGridElement {
 		drawer.drawEllipse(halfWidth, halfHeight, halfWidth-1, halfHeight-1);
 		properties.drawTextForUseCase(getRealSize().width, getRealSize().height);
 	}
+
+
+	 @Override
+	 public StickingPolygon generateStickingBorder(int x, int y, int width, int height) {
+		 //First point is the top left then the points are added clockwise
+	 StickingPolygon p = new StickingPolygon();
 	
+	 p.addPoint(x + width / 4, y);
+	 p.addPoint(x + width * 3 / 4, y);
+				
+	 p.addPoint(x + width, y + height / 4);
+	 p.addPoint(x + width, y + height * 3 / 4);
+				
+	 p.addPoint(x + width * 3 / 4, y + height);
+	 p.addPoint(x + width / 4, y + height);
+				
+	 p.addPoint(x, y + height * 3 / 4);
+	 p.addPoint(x, y + height / 4, true);
+				
+	 return p;
+	 }
 }
 
