a3f98869d2726ae89b4b41d298138654a3512f7b
AFDiaX <AFDiaX@79637776-beb9-4c1a-dc75-bc3ee3110a07>
2012-07-23 08:12:45 +0000

    NewGridElement: vertical and horizontal align are customizable, created settings object for abstraction

---
+++ trunk/Baselet/src/com/umlet/element/experimental/settings/SettingsUseCase.java
@@ -0,0 +1,30 @@
+package com.umlet.element.experimental.settings;
+
+import com.baselet.control.Constants.AlignHorizontal;
+import com.baselet.control.Constants.AlignVertical;
+
+public class SettingsUseCase implements Settings {
+
+	@Override
+	public float[] getXValues(float y, int height, int width) {
+		float b = height/2;
+		float a = width/2;
+		int x = (int) Math.sqrt((1-(Math.pow(b-y, 2) / Math.pow(b, 2)))*Math.pow(a, 2));
+		return new float[] {a-x, a+x};
+	}
+
+	@Override
+	public AlignVertical getVAlign() {
+		return AlignVertical.TOP;
+	}
+
+	@Override
+	public AlignHorizontal getHAlignBeforeLine() {
+		return AlignHorizontal.CENTER;
+	}
+
+	@Override
+	public AlignHorizontal getHAlignAfterLine() {
+		return AlignHorizontal.CENTER;
+	}
+}
--- trunk/Baselet/src/com/umlet/element/experimental/uml/Class.java
+++ trunk/Baselet/src/com/umlet/element/experimental/uml/Class.java
@@ -13,24 +13,7 @@ public class Class extends NewGridElement {
 	public void updateModelFromText() {
 		super.updateModelFromText();
 		drawer.drawRectangle(0, 0, getRealSize().width-1, getRealSize().height-1);
-		drawClassElements(getRealSize().width-1);
-	}
-	
-	public void drawClassElements(int width) {
-		AlignHorizontal align = AlignHorizontal.CENTER;
-		float distanceBetweenTexts = drawer.textHeightWithSpace();
-		float yPos = distanceBetweenTexts;
-		for (String line : properties.getPropertiesTextFiltered()) {
-			if (line.equals("--")) {
-				align = AlignHorizontal.LEFT;
-				float linePos = yPos - (distanceBetweenTexts/2);
-				drawer.drawLine(0, linePos, width, linePos);
-			}
-			else {
-				drawer.print(line, yPos, align);
-			}
-			yPos += distanceBetweenTexts;
-		}
+		properties.drawTextForClass(getRealSize().width, getRealSize().height);
 	}
 }
 
