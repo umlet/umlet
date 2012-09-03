fb4f906c7a3bd850ec78f8e4e9cbcb3eb4ceb72f
AFDiaX <AFDiaX@79637776-beb9-4c1a-dc75-bc3ee3110a07>
2012-09-03 19:39:42 +0000

    added javadoc to settings to explain facets

--- trunk/Baselet/src/com/umlet/element/experimental/settings/Settings.java
+++ trunk/Baselet/src/com/umlet/element/experimental/settings/Settings.java
@@ -25,12 +25,22 @@ public abstract class Settings {
 		return new Facet[]{new DefaultGlobalFacet()};
 	}
 
+	/**
+	 * facets are checked and applied during text parsing.
+	 * e.g. if a line matches "--" and the facet SeparatorLine is setup for the current element,
+	 * a separator line will be drawn instead of printing the text.
+	 */
 	private Facet[] facets;
 	public final Facet[] getFacets() {
 		if (facets == null) facets = createFacets();
 		return facets;
 	}
 
+	/**
+	 * before any other parsing, the properties panel text is parsed for matching global facets.
+	 * therefore any setting made by a global facet will influence every text line, although it could be located at the bottom
+	 * e.g. fg=red could be located at the bottom, but will still be applied to the whole text
+	 */
 	private Facet[] globalFacets;
 	public final Facet[] getGlobalFacets() {
 		if (globalFacets == null) globalFacets = createGlobalFacets();
