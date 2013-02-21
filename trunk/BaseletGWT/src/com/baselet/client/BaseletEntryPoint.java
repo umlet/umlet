d9e62bea8281fdff3412c83ec2813a4248584452
AFDiaX <AFDiaX@79637776-beb9-4c1a-dc75-bc3ee3110a07>
2013-02-21 18:31:32 +0000

    Initial import.

---
+++ trunk/BaseletGWT/src/com/baselet/client/BaseletEntryPoint.java
@@ -0,0 +1,13 @@
+package com.baselet.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class BaseletEntryPoint implements EntryPoint {
+
+	@Override
+	public void onModuleLoad() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
