b2f314114ee9f3933f5f24e3ece95a25b1e6f71a
AFDiaX <AFDiaX@79637776-beb9-4c1a-dc75-bc3ee3110a07>
2013-02-22 16:25:30 +0000

    GWT: own canvas wrapper

--- trunk/BaseletGWT/src/com/baselet/client/DrawPanel.java
+++ trunk/BaseletGWT/src/com/baselet/client/DrawPanel.java
@@ -1,5 +1,8 @@
 package com.baselet.client;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.baselet.client.MouseDragUtils.MouseDragHandler;
 import com.google.gwt.canvas.client.Canvas;
 import com.google.gwt.canvas.dom.client.Context2d;
@@ -43,12 +46,10 @@ public class DrawPanel extends Composite {
 	@UiField
 	ScrollPanel palettePanel;
 
-	CssColor red = CssColor.make("rgba(" + 255 + ", " + 0 + "," + 0 + ", " + 1.0 + ")");
-
 	public DrawPanel() {
 		initWidget(uiBinder.createAndBindUi(this));
 
-		Canvas diagramCanvas = makeCanvas(1500, 1500);
+		Canvas diagramCanvas = new DrawPanelCanvas().makeCanvas(1500, 1500);
 		diagramTabPanel.add(new ScrollPanel(diagramCanvas),"Tab-CANVAS"); 
 		diagramTabPanel.add(new HTML("ONE")," Tab-1 ");
 		diagramTabPanel.add(new HTML("TWO")," Tab-2 ");
@@ -58,50 +59,8 @@ public class DrawPanel extends Composite {
 		paletteChooser.addItem("B");
 		paletteChooser.addItem("C");
 
-		Canvas paletteCanvas = makeCanvas(3000, 3000);
+		Canvas paletteCanvas = new DrawPanelCanvas().makeCanvas(1500, 1500);
 		palettePanel.add(paletteCanvas);
 	}
 
-	private Canvas makeCanvas(int width, int height) {
-		final Canvas canvas = Canvas.createIfSupported();
-		canvas.setStyleName("canvas");
-		//		canvas.setWidth(width + "px");
-		canvas.setCoordinateSpaceWidth(width);
-		//		canvas.setHeight(height + "px");
-		canvas.setCoordinateSpaceHeight(height);
-
-		final Context2d context = canvas.getContext2d();
-
-		MouseDragUtils.addMouseDragHandler(canvas, new MouseDragHandler() {
-			@Override
-			public void onMouseDrag(int diffX, int diffY) {
-				context.translate(diffX, diffY);
-				draw(context);
-			}
-		});
-
-//		canvas.addMouseWheelHandler(new MouseWheelHandler() {
-//			@Override
-//			public void onMouseWheel(MouseWheelEvent event) {
-//				float zoom;
-//				if(event.getDeltaY() < 0) { // scrolling up = negative values, scrolling down = positive values
-//					zoom = 1.1f;
-//				} else {
-//					zoom = 0.9f;
-//				}
-//				context.scale(zoom,zoom);
-//			}
-//		});
-
-		draw(context);
-
-		return canvas;
-	}
-
-	private void draw(final Context2d context) {
-		context.clearRect(-100000, -100000, 200000, 200000);
-		context.setFillStyle(red);
-		context.fillRect(0, 0, 30, 30);
-		context.fill();
-	}
 }
