48f0770c39c4e81ca6449fd1eef421d2dfb1a4b8
AFDiaX <AFDiaX@79637776-beb9-4c1a-dc75-bc3ee3110a07>
2013-02-22 19:01:37 +0000

    GWT: transparent rectangles

--- trunk/BaseletGWT/src/com/baselet/client/GridElement.java
+++ trunk/BaseletGWT/src/com/baselet/client/GridElement.java
@@ -1,18 +1,28 @@
 package com.baselet.client;
 
+import com.google.gwt.canvas.dom.client.CssColor;
+
 public class GridElement {
 
 	Rectangle canvasRectangle;
 	
-	public GridElement(Rectangle bounds) {
-		this.canvasRectangle = bounds;
-	}
+	CssColor color;
 	
+	public GridElement(Rectangle canvasRectangle, CssColor color) {
+		super();
+		this.canvasRectangle = canvasRectangle;
+		this.color = color;
+	}
+
 	public Rectangle getBounds() {
 		return canvasRectangle;
 	}
 	
 	public boolean contains(int x, int y) {
 		return canvasRectangle.contains(x, y);
+	}
+	
+	public CssColor getColor() {
+		return color;
 	}
 }
