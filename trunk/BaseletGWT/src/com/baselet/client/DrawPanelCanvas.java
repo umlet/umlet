6a1977e0530c343e6084f57829d75262b04d3476
AFDiaX <AFDiaX@79637776-beb9-4c1a-dc75-bc3ee3110a07>
2013-02-23 13:02:56 +0000

    GWT: every element has its own canvas to draw (changing one element doesn't force others to refill the canvas object)

--- trunk/BaseletGWT/src/com/baselet/client/DrawPanelCanvas.java
+++ trunk/BaseletGWT/src/com/baselet/client/DrawPanelCanvas.java
@@ -7,32 +7,32 @@ import com.baselet.client.MouseDragUtils.MouseDragHandler;
 import com.google.gwt.canvas.client.Canvas;
 import com.google.gwt.canvas.dom.client.Context2d;
 import com.google.gwt.canvas.dom.client.CssColor;
-import com.google.gwt.canvas.dom.client.FillStrokeStyle;
 
 public class DrawPanelCanvas {
-	
+
 	public final static int GRID_SIZE = 10;
 
 	public static final CssColor RED = CssColor.make("rgba(" + 255 + ", " + 0 + "," + 0 + ", " + 0.3 + ")");
 	public static final CssColor GREEN = CssColor.make("rgba(" + 0 + ", " + 255 + "," + 0 + ", " + 0.3 + ")");
 	public static final CssColor BLUE = CssColor.make("rgba(" + 0 + ", " + 0 + "," + 255 + ", " + 0.3 + ")");
 	public static final CssColor GRAY = CssColor.make("rgba(" + 100 + ", " + 100 + "," + 100 + ", " + 0.2 + ")");
-	
+
 	List<GridElement> gridElements = new ArrayList<GridElement>();
-	
+
 	Canvas elementCanvas;
-	
+
 	Canvas backgroundCanvas;
-	
+
 	public DrawPanelCanvas() {
-		gridElements.add(new GridElement(new Rectangle(10, 10, 30, 30), RED));
-		gridElements.add(new GridElement(new Rectangle(50, 10, 30, 30), GREEN));
-		gridElements.add(new GridElement(new Rectangle(50, 50, 30, 30), RED));
-		gridElements.add(new GridElement(new Rectangle(110, 110, 30, 30), GREEN));
-		gridElements.add(new GridElement(new Rectangle(150, 110, 30, 30), RED));
-		gridElements.add(new GridElement(new Rectangle(150, 150, 30, 30), GREEN));
+		gridElements.add(new GridElement(new Rectangle(10, 10, 30, 30), RED, new CanvasWrapperGWT()));
+		gridElements.add(new GridElement(new Rectangle(50, 10, 30, 30), GREEN, new CanvasWrapperGWT()));
+		gridElements.add(new GridElement(new Rectangle(50, 50, 30, 30), RED, new CanvasWrapperGWT()));
+		gridElements.add(new GridElement(new Rectangle(110, 110, 30, 30), GREEN, new CanvasWrapperGWT()));
+		gridElements.add(new GridElement(new Rectangle(150, 110, 30, 30), RED, new CanvasWrapperGWT()));
+		gridElements.add(new GridElement(new Rectangle(150, 150, 30, 30), GREEN, new CanvasWrapperGWT()));
+
 	}
-	
+
 	Canvas makeCanvas(int width, int height) {
 		elementCanvas = initCanvas(width, height);
 		backgroundCanvas = initCanvas(width, height);
@@ -42,27 +42,27 @@ public class DrawPanelCanvas {
 			public void onMouseDrag(int diffX, int diffY, GridElement gridElement) {
 				if (gridElement == null) { // nothing selected -> move whole diagram
 					for (GridElement ge : gridElements) {
-						ge.getBounds().move(diffX, diffY);
+						ge.move(diffX, diffY);
 					}
 				} else {
-					gridElement.getBounds().move(diffX, diffY);
+					gridElement.move(diffX, diffY);
 				}
 				draw();
 			}
 		});
 
-//		canvas.addMouseWheelHandler(new MouseWheelHandler() {
-//			@Override
-//			public void onMouseWheel(MouseWheelEvent event) {
-//				float zoom;
-//				if(event.getDeltaY() < 0) { // scrolling up = negative values, scrolling down = positive values
-//					zoom = 1.1f;
-//				} else {
-//					zoom = 0.9f;
-//				}
-//				context.scale(zoom,zoom);
-//			}
-//		});
+		//		canvas.addMouseWheelHandler(new MouseWheelHandler() {
+		//			@Override
+		//			public void onMouseWheel(MouseWheelEvent event) {
+		//				float zoom;
+		//				if(event.getDeltaY() < 0) { // scrolling up = negative values, scrolling down = positive values
+		//					zoom = 1.1f;
+		//				} else {
+		//					zoom = 0.9f;
+		//				}
+		//				context.scale(zoom,zoom);
+		//			}
+		//		});
 
 		drawBackgroundGrid(width, height);
 		draw();
@@ -94,17 +94,16 @@ public class DrawPanelCanvas {
 		Context2d context = elementCanvas.getContext2d();
 		context.clearRect(-1000000, -1000000, 2000000, 2000000);
 		for (GridElement ge : gridElements) {
-			context.setFillStyle(ge.getColor());
-			context.fillRect(ge.getBounds().getX(), ge.getBounds().getY(), ge.getBounds().getWidth(), ge.getBounds().getHeight());
+			ge.updateCanvas();
+			((CanvasWrapperGWT) ge.getCanvasUnderlying()).drawOn(context);
 		}
-		context.fill();
 		context.drawImage(backgroundCanvas.getCanvasElement(), 0, 0);
 	}
-	
+
 	public Canvas getCanvas() {
 		return elementCanvas;
 	}
-	
+
 	public GridElement getGridElementOnPosition(int x, int y) {
 		for (GridElement ge : gridElements) {
 			if (ge.contains(x, y)) return ge;
