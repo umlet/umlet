b67a2567211e764462c06f253f98859fbf42ae27
AFDiaX <AFDiaX@79637776-beb9-4c1a-dc75-bc3ee3110a07>
2013-03-14 12:05:51 +0000

    stickingpolygon uses list instead of polygon dimension, point and rectangle have equals

--- Baselet/src/com/baselet/element/Point.java
+++ Baselet/src/com/baselet/element/Point.java
@@ -38,5 +38,25 @@ public class Point {
 	double distY = o.getY() - this.getY();
 	return Math.sqrt(distX * distX + distY * distY);
     }
-	
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + x;
+		result = prime * result + y;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) return true;
+		if (obj == null) return false;
+		if (getClass() != obj.getClass()) return false;
+		Point other = (Point) obj;
+		if (x != other.x) return false;
+		if (y != other.y) return false;
+		return true;
+	}
+    
 }
--- Baselet/src/com/baselet/element/Rectangle.java
+++ Baselet/src/com/baselet/element/Rectangle.java
@@ -98,4 +98,28 @@ public class Rectangle {
 		return true;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + height;
+		result = prime * result + width;
+		result = prime * result + x;
+		result = prime * result + y;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) return true;
+		if (obj == null) return false;
+		if (getClass() != obj.getClass()) return false;
+		Rectangle other = (Rectangle) obj;
+		if (height != other.height) return false;
+		if (width != other.width) return false;
+		if (x != other.x) return false;
+		if (y != other.y) return false;
+		return true;
+	}
+
 }
