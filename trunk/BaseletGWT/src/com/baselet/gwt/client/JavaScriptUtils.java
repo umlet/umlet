349ebbb7912f9fad0b3eed74bfc9ee4cefc9017a
AFDiaX <AFDiaX@79637776-beb9-4c1a-dc75-bc3ee3110a07>
2013-04-05 14:09:02 +0000

    mozilla dashed lines javascript fixed (was buggy in case of solid line)

--- trunk/BaseletGWT/src/com/baselet/gwt/client/JavaScriptUtils.java
+++ trunk/BaseletGWT/src/com/baselet/gwt/client/JavaScriptUtils.java
@@ -3,39 +3,39 @@ package com.baselet.gwt.client;
 
 public class JavaScriptUtils {
 
-	public static void downloadStringAsFile(String filename, String content) {
-		if (getBrowser() == Browser.CHROME) {
-			downloadStringAsFileWithCustomFilename(filename, content);
-		}
-		else downloadStringAsFileWithoutCustomName(content);
-	}
-	
-	private static native void downloadStringAsFileWithoutCustomName(String content)/*-{
-    document.location = 'data:Application/octet-stream,' + content;
-	}-*/;
-
-	/**
-	 * uses download-attribute of anchor tag. currently only supported by chrome
-	 */
-	private static native void downloadStringAsFileWithCustomFilename(String filename, String content)/*-{
-		var a = document.createElement('a');
-		var blob = new Blob([content], {'type':'application\/octet-stream'});
-		a.href = window.URL.createObjectURL(blob);
-		a.download = filename;
-		a.click();
-	}-*/;
-	
-	private enum Browser {
-		CHROME, OTHER;
-	}
-	
-	private static Browser getBrowser() {
-		if (getUserAgent().contains("chrome")) return Browser.CHROME;
-		else return Browser.OTHER;
-	}
-
-	private static native String getUserAgent() /*-{
-	return navigator.userAgent.toLowerCase();
-	}-*/;
+//	public static void downloadStringAsFile(String filename, String content) {
+//		if (getBrowser() == Browser.CHROME) {
+//			downloadStringAsFileWithCustomFilename(filename, content);
+//		}
+//		else downloadStringAsFileWithoutCustomName(content);
+//	}
+//	
+//	private static native void downloadStringAsFileWithoutCustomName(String content)/*-{
+//    document.location = 'data:Application/octet-stream,' + content;
+//	}-*/;
+//
+//	/**
+//	 * uses download-attribute of anchor tag. currently only supported by chrome
+//	 */
+//	private static native void downloadStringAsFileWithCustomFilename(String filename, String content)/*-{
+//		var a = document.createElement('a');
+//		var blob = new Blob([content], {'type':'application\/octet-stream'});
+//		a.href = window.URL.createObjectURL(blob);
+//		a.download = filename;
+//		a.click();
+//	}-*/;
+//	
+//	private enum Browser {
+//		CHROME, OTHER;
+//	}
+//	
+//	private static Browser getBrowser() {
+//		if (getUserAgent().contains("chrome")) return Browser.CHROME;
+//		else return Browser.OTHER;
+//	}
+//
+//	private static native String getUserAgent() /*-{
+//	return navigator.userAgent.toLowerCase();
+//	}-*/;
 
 }
