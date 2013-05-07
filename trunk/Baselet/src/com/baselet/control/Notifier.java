8d4ac75214e13bf6fd9d980499fffeb61aff31a2
AFDiaX <AFDiaX@79637776-beb9-4c1a-dc75-bc3ee3110a07>
2013-05-07 06:37:41 +0000

    Issue 137: Notification sometimes does not disappear is fixed

--- trunk/Baselet/src/com/baselet/control/Notifier.java
+++ trunk/Baselet/src/com/baselet/control/Notifier.java
@@ -11,52 +11,34 @@ import com.baselet.diagram.draw.swing.Converter;
 
 public class Notifier {
 
-	DrawPanel notifierPanel;
-
-	private DiagramNotification notification;
-	private TimerTask notificationRemoveTask;
-	
 	private static final Notifier instance = new Notifier();
-	
+
 	public synchronized static Notifier getInstance() {
 		return instance;
 	}
 
-
-	public void setNotificationPanel(DrawPanel notifierPanel) {
-		this.notifierPanel = notifierPanel;
-	}
-
-
 	public void showNotification(final String message) {
-	SwingUtilities.invokeLater(new Runnable() {
-		@Override
-		public void run() {
-			showNotificationHelper(message);
-		}
-	});}
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				showNotificationHelper(message);
+			}
+		});}
 
 
 	private void showNotificationHelper(String message) {
-		if (notification != null) {
-			notifierPanel.remove(notification);
-				if (notificationRemoveTask != null) notificationRemoveTask.cancel();
-		}
+		final DrawPanel notifierPanel = Main.getInstance().getDiagramHandler().getDrawPanel();
 
 		Rectangle viewRect = notifierPanel.getScrollPane().getViewport().getViewRect();
-		notification = new DiagramNotification(Converter.convert(viewRect), message);
-		notificationRemoveTask = new TimerTask() {
+		final DiagramNotification notification = new DiagramNotification(Converter.convert(viewRect), message);
+		new Timer().schedule(new TimerTask() {
 			@Override
 			public void run() {
-				if (notification != null) {
-					notifierPanel.remove(notification);
-					notifierPanel.repaint();
-				}
+				notifierPanel.remove(notification);
+				notifierPanel.repaint();
 			}
-		};
-		new Timer().schedule(notificationRemoveTask, Constants.NOTIFICATION_SHOW_TIME);
-		
-		notifierPanel.add(notification);
-		notifierPanel.repaint();
+		}, Constants.NOTIFICATION_SHOW_TIME);
+
+		notifierPanel.setNotification(notification);
 	}
 }
