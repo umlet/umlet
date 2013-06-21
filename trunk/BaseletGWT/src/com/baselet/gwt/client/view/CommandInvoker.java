498b4698221dc446d4bc04411768e4b952bbba57
AFDiaX <AFDiaX@79637776-beb9-4c1a-dc75-bc3ee3110a07>
2013-06-21 13:49:51 +0000

    basic copy&paste within one diagram implemented (ctrl+c/ctrl+v)

--- trunk/BaseletGWT/src/com/baselet/gwt/client/view/CommandInvoker.java
+++ trunk/BaseletGWT/src/com/baselet/gwt/client/view/CommandInvoker.java
@@ -1,7 +1,11 @@
 package com.baselet.gwt.client.view;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
 
+import com.baselet.control.NewGridElementConstants;
 import com.baselet.diagram.commandnew.AddGridElementCommand;
 import com.baselet.diagram.commandnew.Controller;
 import com.baselet.diagram.commandnew.RemoveGridElementCommand;
@@ -19,6 +23,10 @@ public class CommandInvoker extends Controller {
 	void addElements(GridElement ... elements) {
 		this.executeCommand(new AddGridElementCommand(canvas, canvas.getSelector(), elements));
 	}
+
+	void addElements(Collection<GridElement> elements) {
+		addElements(elements.toArray(new GridElement[elements.size()]));
+	}
 	
 	void removeElements(GridElement ... elements) {
 		this.executeCommand(new RemoveGridElementCommand(canvas, canvas.getSelector(), elements));
@@ -26,5 +34,29 @@ public class CommandInvoker extends Controller {
 
 	void removeElements(Collection<GridElement> elements) {
 		removeElements(elements.toArray(new GridElement[elements.size()]));
+	}
+	
+	
+	
+	
+	
+	//TODO implement copy & paste as commands
+
+	private List<GridElement> copyTemplates = new ArrayList<GridElement>();
+	void copyElements(Collection<GridElement> elements) {
+		copyTemplates = copyElementsInList(elements); // must be copied here to ensure location etc. will not be changed
+	}
+	
+	void pasteElements() {
+		addElements(copyElementsInList(copyTemplates)); // copy here to make sure it can be pasted multiple times
+	}
+
+	private List<GridElement> copyElementsInList(Collection<GridElement> sourceElements) {
+		List<GridElement> targetElements = new ArrayList<GridElement>();
+		for (GridElement ge : sourceElements) {
+			GridElement e = ge.CloneFromMe();
+			targetElements.add(e);
+		}
+		return targetElements;
 	}
 }
