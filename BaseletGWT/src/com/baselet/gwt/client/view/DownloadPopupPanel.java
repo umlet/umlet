37ee2f53355d8f1d8b4bd378e44dacc0b16c73c1
AFDiaX <AFDiaX@79637776-beb9-4c1a-dc75-bc3ee3110a07>
2013-06-26 14:04:36 +0000

    export popup closes on esc

--- BaseletGWT/src/com/baselet/gwt/client/view/DownloadPopupPanel.java
+++ BaseletGWT/src/com/baselet/gwt/client/view/DownloadPopupPanel.java
@@ -1,21 +1,37 @@
 package com.baselet.gwt.client.view;
+import com.google.gwt.event.dom.client.KeyCodes;
+import com.google.gwt.user.client.Event;
+import com.google.gwt.user.client.Event.NativePreviewEvent;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.PopupPanel;
 import com.google.gwt.user.client.ui.SimplePanel;
 
 
-public class DownloadPopupPanel {
+public class DownloadPopupPanel extends PopupPanel {
 	
 	public DownloadPopupPanel(String uxfUrl, String pngUrl) {
-		PopupPanel popup = new PopupPanel(true);
-		popup.setGlassEnabled(true);
-		popup.setStyleName("popup");
+		super(true);
+		setGlassEnabled(true);
+		setStyleName("popup");
 		String html =
 				"<p>\"Right click -&gt; Save as\" on the following links</p>" +
 				"<p><a href='" + uxfUrl + "'>Diagram File</a></p>" +
 				"<p><a href='" + pngUrl + "'>Image File</a></p>";
 		SimplePanel panel = new SimplePanel(new HTML(html));
-		popup.setWidget(panel);
-		popup.center();
+		setWidget(panel);
+		center();
+	}
+
+	/**
+	 * pressing ESC closes the dialogbox
+	 */
+	@Override
+	protected void onPreviewNativeEvent(NativePreviewEvent event) {
+		super.onPreviewNativeEvent(event);
+		if (
+				event.getTypeInt() == Event.ONKEYDOWN && 
+				event.getNativeEvent().getKeyCode() == KeyCodes.KEY_ESCAPE) {
+			hide();
+		}
 	}
 }
