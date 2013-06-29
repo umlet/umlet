964eef626cc471e03f7a83f33cd75ed8808c3128
AFDiaX <AFDiaX@79637776-beb9-4c1a-dc75-bc3ee3110a07>
2013-06-29 12:36:17 +0000

    selector helper methods

--- trunk/BaseletElements/src/com/baselet/element/Selector.java
+++ trunk/BaseletElements/src/com/baselet/element/Selector.java
@@ -15,9 +15,17 @@ public abstract class Selector {
 		select(elements);
 	}
 
-//	public void selectOnly(Collection<GridElement> elements) {
-//		selectOnly(elements.toArray(new GridElement[elements.size()]));
-//	}
+	public void selectOnly(Collection<GridElement> elements) {
+		selectOnly(elements.toArray(new GridElement[elements.size()]));
+	}
+
+	public void select(Collection<GridElement> elements) {
+		select(elements.toArray(new GridElement[elements.size()]));
+	}
+
+	public void deselect(Collection<GridElement> elements) {
+		deselect(elements.toArray(new GridElement[elements.size()]));
+	}
 
 	public void deselectAll() {
 		deselect(getSelectedElements().toArray(new GridElement[getSelectedElements().size()]));
