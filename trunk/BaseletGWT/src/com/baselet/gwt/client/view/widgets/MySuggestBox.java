3b3edebc29f2bc3e30d88b2d64d7fb1162ea7c11
AFDiaX <AFDiaX@79637776-beb9-4c1a-dc75-bc3ee3110a07>
2013-06-30 09:33:06 +0000

    gwt: suggestbox has own oracle and more beautiful suggestions styling

--- trunk/BaseletGWT/src/com/baselet/gwt/client/view/widgets/MySuggestBox.java
+++ trunk/BaseletGWT/src/com/baselet/gwt/client/view/widgets/MySuggestBox.java
@@ -8,21 +8,19 @@ import com.google.gwt.user.client.ui.SuggestBox;
 import com.google.gwt.user.client.ui.SuggestOracle;
 
 public class MySuggestBox extends SuggestBox {
-	
-	//TODO perhaps use '\t' because its completely invisible
-	public final static String COMMENT_CHAR = " ~";
 
 	public MySuggestBox(SuggestOracle oracle, OwnTextArea textArea) {
 		super(oracle, textArea);
 		
-		// if the focus is on the suggestbox avoid propagating certain keyevents to the textarea
 		this.addKeyPressHandler(new KeyPressHandler() {
 	        @Override
 	        public void onKeyPress(KeyPressEvent event) {
 	            int key = event.getNativeEvent().getKeyCode();
-	            boolean isSuggestBoxControlKey = key == KeyCodes.KEY_ENTER || key == KeyCodes.KEY_UP || key == KeyCodes.KEY_DOWN;
-				if (MySuggestBox.this.isSuggestionListShowing() && isSuggestBoxControlKey) {
-	            	event.getNativeEvent().preventDefault();
+	            if (key == KeyCodes.KEY_ESCAPE) {
+	            	MySuggestBox.this.hideSuggestionList();
+	            }
+	            else if (MySuggestBox.this.isSuggestionListShowing() && (key == KeyCodes.KEY_ENTER || key == KeyCodes.KEY_UP || key == KeyCodes.KEY_DOWN)) {
+	            	event.getNativeEvent().preventDefault(); // if the focus is on the suggestbox avoid propagating certain keyevents to the textarea
 	            }
 	        }
 	});
@@ -30,8 +28,7 @@ public class MySuggestBox extends SuggestBox {
 
 	@Override
 	public void setText(String text) {
-		// only replace current line. Also only use suggestion until COMMENT_CHAR which only contains additional explaination about the gridelement-setting
-		super.setText(replaceTextOfCurrentLine(text.substring(0, text.indexOf(COMMENT_CHAR))));
+		super.setText(replaceTextOfCurrentLine(text));
 	}
 
 	@Override
@@ -67,7 +64,7 @@ public class MySuggestBox extends SuggestBox {
 			boolean replaced = false;
 			int cursorPos = getValueBox().getCursorPos();
 			int currentPos = 0;
-			for (String line : wholeText.split("(\r)?\n")) {
+			for (String line : wholeText.split("(\r)?\n", -1)) {
 				currentPos += line.length()+1;
 				if (cursorPos < currentPos && !replaced) {
 					returnText += newText + "\n";
---
+++ trunk/BaseletGWT/src/com/baselet/gwt/client/view/widgets/MySuggestOracle.java
@@ -0,0 +1,87 @@
+package com.baselet.gwt.client.view.widgets;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+
+import com.baselet.gui.AutocompletionText;
+import com.google.gwt.user.client.ui.SuggestOracle;
+
+public class MySuggestOracle extends SuggestOracle {
+
+	private static class MySuggestion implements Suggestion {
+		
+		private AutocompletionText autoCompletionText;
+		
+		public MySuggestion(AutocompletionText autoCompletionText) {
+			this.autoCompletionText = autoCompletionText;
+		}
+
+		@Override
+		public String getDisplayString() {
+			return autoCompletionText.getText() + " <span style='font-style:italic;font-size:0.8em;color:gray'>" + autoCompletionText.getInfo() + "</span>";
+		}
+
+		@Override
+		public String getReplacementString() {
+			return autoCompletionText.getText();
+		}
+
+	}
+
+	@Override
+	public boolean isDisplayStringHTML() {
+		return true;
+	}
+	
+	private List<Suggestion> suggestions = new ArrayList<Suggestion>();
+
+	public void requestSuggestions(final Request request, final Callback callback) {
+		String userInput = request.getQuery();
+		Collection<Suggestion> result = new LinkedList<Suggestion>();
+		for (Suggestion suggestion : suggestions) {
+			if (suggestion.getReplacementString().startsWith(userInput)) {
+				result.add(highlightUserInput(suggestion, userInput));
+			}
+		}
+		Response response = new Response(result);
+		callback.onSuggestionsReady(request, response);
+	}
+
+	private Suggestion highlightUserInput(final Suggestion suggestion, final String userInput) {
+		return new Suggestion() {
+			@Override
+			public String getReplacementString() {
+				return suggestion.getReplacementString();
+			}
+			@Override
+			public String getDisplayString() {
+				return "<strong>" + userInput + "</strong>" + suggestion.getDisplayString().substring(userInput.length());
+			}
+		};
+	}
+
+	public void setAutocompletionList(List<AutocompletionText> autocompletionList) {
+		suggestions.clear();
+		for (AutocompletionText text : autocompletionList) {
+			suggestions.add(new MySuggestion(text));
+		}
+	}
+
+	private boolean showAllAsDefault = false;
+	
+	public void setShowAllAsDefault(boolean showAllAsDefault) {
+		this.showAllAsDefault = showAllAsDefault;
+	}
+	
+	@Override
+	public void requestDefaultSuggestions(final Request request, final Callback callback) {
+		if (showAllAsDefault) {
+			callback.onSuggestionsReady(request, new Response(suggestions));
+		} else {
+			super.requestDefaultSuggestions(request, callback);
+		}
+	}
+}
