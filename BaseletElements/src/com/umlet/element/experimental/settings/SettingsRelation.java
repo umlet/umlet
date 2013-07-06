c556f818a8fe072ef20ddbaf664a608da854eb27
AFDiaX <AFDiaX@79637776-beb9-4c1a-dc75-bc3ee3110a07>
2013-07-06 10:50:15 +0000

    new relation: arrow is created from parsed facet

--- BaseletElements/src/com/umlet/element/experimental/settings/SettingsRelation.java
+++ BaseletElements/src/com/umlet/element/experimental/settings/SettingsRelation.java
@@ -9,8 +9,21 @@ import com.umlet.element.experimental.settings.facets.InnerClass;
 import com.umlet.element.experimental.settings.facets.SeparatorLine;
 import com.umlet.element.experimental.settings.facets.TemplateClass;
 import com.umlet.element.experimental.settings.facets.DefaultGlobalFacet.ElementStyleEnum;
+import com.umlet.element.experimental.uml.relation.ArrowEnd;
+import com.umlet.element.experimental.uml.relation.ArrowStart;
+import com.umlet.element.experimental.uml.relation.RelationPoints;
 
 public class SettingsRelation extends Settings {
+
+	private RelationPoints relationPoints;
+
+	public SettingsRelation(RelationPoints relationPoints) {
+		this.relationPoints = relationPoints;
+	}
+	
+	public RelationPoints getRelationPoints() {
+		return relationPoints;
+	}
 
 	@Override
 	public XValues getXValues(float y, int height, int width) {
@@ -34,7 +47,7 @@ public class SettingsRelation extends Settings {
 
 	@Override
 	public Facet[] createFacets() {
-		return new Facet[0];
+		return new Facet[] {new ArrowStart(), new ArrowEnd()};
 	}
 
 }
--- BaseletElements/src/com/umlet/element/experimental/uml/relation/ArrowEnd.java
+++ BaseletElements/src/com/umlet/element/experimental/uml/relation/ArrowEnd.java
@@ -1,11 +1,32 @@
 package com.umlet.element.experimental.uml.relation;
 
+import java.util.List;
+
 import com.baselet.diagram.draw.BaseDrawHandler;
 import com.baselet.diagram.draw.geom.Line;
+import com.umlet.element.experimental.PropertiesConfig;
 
 public class ArrowEnd extends Arrow {
-
-	public static void draw(BaseDrawHandler drawer, Line line) {
+	
+	public void draw(BaseDrawHandler drawer, Line line) {
 		drawArrowToLine(drawer, line, false);
 	}
+
+	@Override
+	public String getKey() {
+		return "end=>";
+	}
+	
+	@Override
+	public String getDescription() {
+		return "draw normal arrow";
+	}
+
+	@Override
+	public void handleLine(String line, BaseDrawHandler drawer, PropertiesConfig propConfig) {
+		List<Line> linesToDraw = getLinesFromConfig(propConfig);
+		Line lineToDraw = linesToDraw.get(linesToDraw.size()-1);
+		drawArrowToLine(drawer, lineToDraw, false);
+	}
+	
 }
