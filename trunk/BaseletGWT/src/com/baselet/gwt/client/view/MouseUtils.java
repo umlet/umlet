86b915a7b5b821c171330e79ce045bf487c2ef88
AFDiaX <AFDiaX@79637776-beb9-4c1a-dc75-bc3ee3110a07>
2013-08-18 12:01:26 +0000

    gwt: contextmenu is shown on touchdevices after touching 500ms without moving

--- trunk/BaseletGWT/src/com/baselet/gwt/client/view/MouseUtils.java
+++ trunk/BaseletGWT/src/com/baselet/gwt/client/view/MouseUtils.java
@@ -5,6 +5,10 @@ import java.util.Arrays;
 import com.baselet.control.NewGridElementConstants;
 import com.baselet.diagram.draw.geom.Point;
 import com.baselet.element.GridElement;
+import com.google.gwt.event.dom.client.ContextMenuEvent;
+import com.google.gwt.event.dom.client.ContextMenuHandler;
+import com.google.gwt.event.dom.client.DoubleClickEvent;
+import com.google.gwt.event.dom.client.DoubleClickHandler;
 import com.google.gwt.event.dom.client.HumanInputEvent;
 import com.google.gwt.event.dom.client.MouseDownEvent;
 import com.google.gwt.event.dom.client.MouseDownHandler;
@@ -13,6 +17,8 @@ import com.google.gwt.event.dom.client.MouseMoveEvent;
 import com.google.gwt.event.dom.client.MouseMoveHandler;
 import com.google.gwt.event.dom.client.MouseOutEvent;
 import com.google.gwt.event.dom.client.MouseOutHandler;
+import com.google.gwt.event.dom.client.MouseOverEvent;
+import com.google.gwt.event.dom.client.MouseOverHandler;
 import com.google.gwt.event.dom.client.MouseUpEvent;
 import com.google.gwt.event.dom.client.MouseUpHandler;
 import com.google.gwt.event.dom.client.TouchEndEvent;
@@ -23,6 +29,7 @@ import com.google.gwt.event.dom.client.TouchMoveHandler;
 import com.google.gwt.event.dom.client.TouchStartEvent;
 import com.google.gwt.event.dom.client.TouchStartHandler;
 import com.google.gwt.user.client.Element;
+import com.google.gwt.user.client.Timer;
 import com.google.gwt.user.client.ui.FocusWidget;
 
 public class MouseUtils {
@@ -35,6 +42,15 @@ public class MouseUtils {
 		private DragStatus dragging = DragStatus.NO;
 		private Point moveStart;
 		private GridElement elementToDrag;
+
+		/**
+		 * doubleclicks are only handled if the mouse has moved into the canvas before
+		 * this is necessary to void unwanted propagation of suggestbox-selections via doubleclick
+		 * TODO: a better fix would be a custom SuggestDisplay which stops mouseevent propagation after handling them
+		 */
+		private boolean doubleClickEnabled = true;
+
+		private Timer menuShowTimer;
 	}
 
 	public interface MouseDragHandler {
@@ -45,63 +61,106 @@ public class MouseUtils {
 		void onMouseDown(GridElement gridElement, boolean controlKeyDown);
 
 		void onMouseMove(Point absolute);
+
+		void onDoubleClick(GridElement ge);
+
+		void onShowMenu(Point point);
 	}
 
-	public static void addMouseHandler(final DrawFocusPanel drawPanelCanvas, FocusWidget canvas, final MouseDragHandler mouseDragHandler) {
+	public static void addMouseHandler(final DrawFocusPanel drawPanelCanvas, FocusWidget canvas, final MouseDragHandler handler) {
 		final Element canvEl = drawPanelCanvas.getElement();
 		final DragCache storage = new DragCache();
+
 		canvas.addTouchStartHandler(new TouchStartHandler() {
 			@Override
-			public void onTouchStart(TouchStartEvent event) {
+			public void onTouchStart(final TouchStartEvent event) {
 				if (event.getTouches().length() == 1) { // only handle single finger touches (to allow zooming with 2 fingers)
-					handleStart(drawPanelCanvas, mouseDragHandler, storage, event, getTouchPoint(canvEl, event));
+					handleStart(drawPanelCanvas, handler, storage, event, getTouchPoint(canvEl, event));
+
+					final Point absolutePos = new Point(event.getTouches().get(0).getPageX(), event.getTouches().get(0).getPageY());
+//					Notification.showInfo("START " + absolutePos.x);
+					storage.menuShowTimer = new Timer() {
+						@Override
+						public void run() {
+							handleShowMenu(handler, absolutePos);
+						}
+					};
+					storage.menuShowTimer.schedule(500);
 				}
 			}
 		});
 		canvas.addMouseDownHandler(new MouseDownHandler() {
 			@Override
 			public void onMouseDown(MouseDownEvent event) {
-				handleStart(drawPanelCanvas, mouseDragHandler, storage, event, getMousePoint(canvEl, event));
+				handleStart(drawPanelCanvas, handler, storage, event, getMousePoint(canvEl, event));
 			}
 		});
 
 		canvas.addTouchEndHandler(new TouchEndHandler() {
 			@Override
 			public void onTouchEnd(TouchEndEvent event) {
-				handleEnd(storage, mouseDragHandler);
+				storage.menuShowTimer.cancel();
+				handleEnd(storage, handler);
 			}
 		});
 		canvas.addMouseUpHandler(new MouseUpHandler() {
 			@Override
 			public void onMouseUp(MouseUpEvent event) {
-				handleEnd(storage, mouseDragHandler);
+				handleEnd(storage, handler);
 			}
 		});
 		canvas.addMouseOutHandler(new MouseOutHandler() {
 			@Override
 			public void onMouseOut(MouseOutEvent event) {
-				handleEnd(storage, mouseDragHandler);
+				handleEnd(storage, handler);
+				storage.doubleClickEnabled = false;
+			}
+		});
+		canvas.addMouseOverHandler(new MouseOverHandler() {
+			@Override
+			public void onMouseOver(MouseOverEvent event) {
+				storage.doubleClickEnabled = true;
 			}
 		});
 
 		canvas.addMouseMoveHandler(new MouseMoveHandler() {
 			@Override
 			public void onMouseMove(MouseMoveEvent event) {
-				handleMove(drawPanelCanvas, mouseDragHandler, storage, event, getMousePoint(canvEl, event));
+				handleMove(drawPanelCanvas, handler, storage, event, getMousePoint(canvEl, event));
 			}
 		});
 		canvas.addTouchMoveHandler(new TouchMoveHandler() {
 			@Override
 			public void onTouchMove(TouchMoveEvent event) {
+				storage.menuShowTimer.cancel();
 				if (event.getTouches().length() == 1) { // only handle single finger touches (to allow zooming with 2 fingers)
-					handleMove(drawPanelCanvas, mouseDragHandler, storage, event, getTouchPoint(canvEl, event));
+					handleMove(drawPanelCanvas, handler, storage, event, getTouchPoint(canvEl, event));
 				}
 			}
 		});
+
+		// double tap on mobile devices is not easy to implement because browser zoom on double-tap is not an event which can be canceled
+		canvas.addDoubleClickHandler(new DoubleClickHandler() {
+			@Override
+			public void onDoubleClick(DoubleClickEvent event) {
+				if (storage.doubleClickEnabled) {
+					handleDoubleClick(drawPanelCanvas, handler, getMousePoint(canvEl, event));
+				}
+			}
+		});
+
+		canvas.addDomHandler(new ContextMenuHandler() {
+			@Override
+			public void onContextMenu(ContextMenuEvent event) {
+				event.preventDefault();
+				event.stopPropagation();
+				handleShowMenu(handler, new Point(event.getNativeEvent().getClientX(), event.getNativeEvent().getClientY()));
+			}
+		}, ContextMenuEvent.getType());
 	}
 
 	private static void handleEnd(final DragCache storage, MouseDragHandler mouseDragHandler) {
-//		Notification.showInfo("UP");
+		//		Notification.showInfo("UP");
 		if (Arrays.asList(DragStatus.FIRST, DragStatus.CONTINUOUS).contains(storage.dragging)) {
 			mouseDragHandler.onMouseDragEnd(storage.elementToDrag);
 		}
@@ -109,7 +168,7 @@ public class MouseUtils {
 	}
 
 	private static void handleStart(final DrawFocusPanel drawPanelCanvas, final MouseDragHandler mouseDragHandler, final DragCache storage, HumanInputEvent<?> event, Point p) {
-//		Notification.showInfo("DOWN " + p.x);
+		//		Notification.showInfo("DOWN " + p.x);
 		storage.moveStart = new Point(p.x, p.y);
 		storage.dragging = DragStatus.FIRST;
 		storage.elementToDrag = drawPanelCanvas.getGridElementOnPosition(storage.moveStart);
@@ -117,7 +176,7 @@ public class MouseUtils {
 	}
 
 	private static void handleMove(final DrawFocusPanel drawPanelCanvas, final MouseDragHandler mouseDragHandler, final DragCache storage, HumanInputEvent<?> event, Point p) {
-//		Notification.showInfo("MOVE " + p.x);
+		//		Notification.showInfo("MOVE " + p.x);
 		event.preventDefault(); // necessary to avoid Chrome showing the text-cursor during dragging
 		if (Arrays.asList(DragStatus.FIRST, DragStatus.CONTINUOUS).contains(storage.dragging)) {
 			int diffX = p.x - storage.moveStart.getX();
@@ -133,6 +192,14 @@ public class MouseUtils {
 		else {
 			mouseDragHandler.onMouseMove(p);
 		}
+	}
+
+	private static void handleDoubleClick(final DrawFocusPanel drawPanelCanvas, final MouseDragHandler handler, Point p) {
+		handler.onDoubleClick(drawPanelCanvas.getGridElementOnPosition(p));
+	}
+
+	private static void handleShowMenu(final MouseDragHandler handler, Point p) {
+		handler.onShowMenu(p);
 	}
 
 	private static Point getMousePoint(final Element canvEl, MouseEvent<?> event) {
