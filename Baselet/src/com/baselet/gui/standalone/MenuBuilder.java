95eb79e256b3c857b67dba964f794ea07d3694f6
AFDiaX <AFDiaX@79637776-beb9-4c1a-dc75-bc3ee3110a07>
2014-02-09 10:24:27 +0000

    copy is before cut in menu added copy/cut/paste where it fits in contextmenu

--- Baselet/src/com/baselet/gui/standalone/MenuBuilder.java
+++ Baselet/src/com/baselet/gui/standalone/MenuBuilder.java
@@ -70,8 +70,8 @@ public class MenuBuilder {
 		editMenu.add(editGroup = menuFactory.createGroup());
 		editMenu.add(editUngroup = menuFactory.createUngroup());
 		editMenu.addSeparator();
-		editMenu.add(editCut = menuFactory.createCut());
 		editMenu.add(editCopy = menuFactory.createCopy());
+		editMenu.add(editCut = menuFactory.createCut());
 		editMenu.add(editPaste = menuFactory.createPaste());
 		menu.add(editMenu);
 		editDelete.setEnabled(false);
