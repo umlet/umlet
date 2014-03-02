deedbcbc9e6a108db3d4ab1c8fd0c992969d5a91
AFDiaX <AFDiaX@79637776-beb9-4c1a-dc75-bc3ee3110a07>
2014-03-02 18:52:40 +0000

    stickingpolygon color is brighter for better distinction to fg-selection-color stickingpolygon for template class implemented

---
+++ trunk/BaseletElements/src/com/baselet/element/sticking/PointDoubleStickingPolygonGenerator.java
@@ -0,0 +1,23 @@
+package com.baselet.element.sticking;
+
+import java.util.List;
+
+import com.baselet.diagram.draw.geom.PointDouble;
+import com.baselet.diagram.draw.geom.Rectangle;
+
+public class PointDoubleStickingPolygonGenerator implements StickingPolygonGenerator {
+	
+	private List<PointDouble> points;
+	
+	public PointDoubleStickingPolygonGenerator(List<PointDouble> points) {
+		super();
+		this.points = points;
+	}
+
+	@Override
+	public StickingPolygon generateStickingBorder(Rectangle rect) {
+		StickingPolygon p = new StickingPolygon(rect.x, rect.y);
+		p.addPoint(points);
+		return p;
+	}
+}
--- trunk/BaseletElements/src/com/umlet/element/experimental/facet/specific/TemplateClassFacet.java
+++ trunk/BaseletElements/src/com/umlet/element/experimental/facet/specific/TemplateClassFacet.java
@@ -1,5 +1,9 @@
 package com.umlet.element.experimental.facet.specific;
 
+import java.util.Arrays;
+import java.util.List;
+
+import com.baselet.control.SharedUtils;
 import com.baselet.control.enumerations.AlignHorizontal;
 import com.baselet.control.enumerations.LineType;
 import com.baselet.diagram.draw.BaseDrawHandler;
@@ -18,7 +22,7 @@ public class TemplateClassFacet extends AbstractGlobalKeyValueFacet {
 	public static final int UPPER_SPACE = 3;
 	public static final int LOWER_SPACE = 3;
 	public static final int LEFT_SPACE = 6;
-	
+
 	@Override
 	public KeyValue getKeyValue() {
 		return new KeyValue("template", new ValueInfo("text", "print template rectangle on top right corner"));
@@ -28,39 +32,48 @@ public class TemplateClassFacet extends AbstractGlobalKeyValueFacet {
 	public void handleValue(String value, BaseDrawHandler drawer, PropertiesConfig propConfig) {
 		double height = drawer.textHeight() + UPPER_SPACE + LOWER_SPACE;
 		double width = drawer.textWidth(value) + LEFT_SPACE;
-		propConfig.addToRightBuffer(width/2);
-		propConfig.setMinTopBuffer(height/2);
+		propConfig.addToRightBuffer(round(width/2));
+		propConfig.setMinTopBuffer(round(height/2));
 		int elemWidth = propConfig.getGridElementSize().width;
 		drawer.setDrawDelayed(true);
 		drawer.print(value, elemWidth - drawer.getDistanceHorizontalBorderToText(), height-LOWER_SPACE, AlignHorizontal.RIGHT);
 		drawer.setDrawDelayed(false);
 		propConfig.putFacetResponse(TemplateClassFacet.class, new Rectangle(elemWidth - width, 0.0, width, height));
 	}
+	
+	private static int round(double val) {
+		return SharedUtils.realignToGrid(false, val, true);
+	}
 
 
-	public static void drawTemplateClass(BaseDrawHandler drawer, Rectangle tR, int height, int width) {
-		// DRAW BACKGROUND RECT
+	public static List<PointDouble> drawTemplateClass(BaseDrawHandler drawer, Rectangle tR, int height, int width) {
 		Style style = drawer.getCurrentStyle();
 		drawer.setForegroundColor(ColorOwn.TRANSPARENT);
-		PointDouble p1 = new PointDouble(0, tR.getHeight()/2);
-		PointDouble p2 = new PointDouble(tR.getX(), tR.getHeight()/2);
-		PointDouble p3 = new PointDouble(tR.getX(), 0);
-		PointDouble p4 = new PointDouble(width, 0);
-		PointDouble p5 = new PointDouble(width, tR.getHeight());
-		PointDouble p6 = new PointDouble(width-tR.getWidth()/2, tR.getHeight());
-		PointDouble p7 = new PointDouble(width-tR.getWidth()/2, height);
-		PointDouble p8 = new PointDouble(0, height);
-		drawer.drawLines(p1, p2, p3, p4, p5, p6, p7, p8, p1);
+		int classTopEnd = round(tR.getHeight()/2);
+		int classWidth = width-round(tR.getWidth()/2);
+		List<PointDouble> p = Arrays.asList(
+				new PointDouble(0, classTopEnd),
+				new PointDouble(tR.getX(), classTopEnd),
+				new PointDouble(tR.getX(), 0),
+				new PointDouble(width, 0),
+				new PointDouble(width, tR.getHeight()),
+				new PointDouble(classWidth, tR.getHeight()),
+				new PointDouble(classWidth, height),
+				new PointDouble(0, height));
+		// DRAW BACKGROUND RECT
+		int i = 0;
+		drawer.drawLines(p.get(i++), p.get(i++), p.get(i++), p.get(i++), p.get(i++), p.get(i++), p.get(i++), p.get(i++), p.get(0));
 		drawer.setStyle(style.cloneFromMe()); // reset style to state before manipulations
 		// DRAW RIGHT RECT
 		drawer.setLineType(LineType.DASHED);
 		drawer.setBackgroundColor(ColorOwn.TRANSPARENT);
 		drawer.drawRectangle(tR);
 		drawer.setStyle(style); // reset style to state before manipulations
-		// DRAW REST OF CLASS
-		drawer.drawLines(p2, p1, p8, p7, p6);
+		// DRAW PARTIAL CLASS BORDER
+		drawer.drawLines(p.get(1), p.get(0), p.get(7), p.get(6), p.get(5));
+		return p;
 	}
-	
+
 	@Override
 	public Priority getPriority() {
 		return Priority.HIGH;
