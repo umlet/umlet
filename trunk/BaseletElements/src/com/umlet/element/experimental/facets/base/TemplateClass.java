8dfef9ba72074eb1dfe85a8f4644138ecb2445be
AFDiaX <AFDiaX@79637776-beb9-4c1a-dc75-bc3ee3110a07>
2014-03-02 13:29:44 +0000

    template class facet reworked

--- trunk/BaseletElements/src/com/umlet/element/experimental/facets/base/TemplateClass.java
+++ trunk/BaseletElements/src/com/umlet/element/experimental/facets/base/TemplateClass.java
@@ -1,48 +1,44 @@
 package com.umlet.element.experimental.facets.base;
 
-import java.util.Arrays;
-import java.util.List;
-
 import com.baselet.control.enumerations.AlignHorizontal;
 import com.baselet.control.enumerations.LineType;
 import com.baselet.diagram.draw.BaseDrawHandler;
+import com.baselet.diagram.draw.geom.PointDouble;
 import com.baselet.diagram.draw.helper.ColorOwn;
 import com.baselet.diagram.draw.helper.Style;
-import com.baselet.gui.AutocompletionText;
 import com.umlet.element.experimental.PropertiesConfig;
-import com.umlet.element.experimental.facets.AbstractGlobalFacet;
+import com.umlet.element.experimental.facets.AbstractGlobalKeyValueFacet;
 
-public class TemplateClass extends AbstractGlobalFacet {
-	
+public class TemplateClass extends AbstractGlobalKeyValueFacet {
+
 	public static TemplateClass INSTANCE = new TemplateClass();
 	private TemplateClass() {}
 
-	private static final String KEY = "template" + SEP;
-	
-	private static final int SPACE = 6;
+	private static final int UPPER_SPACE = 3;
+	private static final int LOWER_SPACE = 3;
+	private static final int LEFT_SPACE = 6;
 	
 	@Override
-	public boolean checkStart(String line) {
-		return line.startsWith(KEY);
+	public KeyValue getKeyValue() {
+		return new KeyValue("template", new ValueInfo("text", "print template rectangle on top right corner"));
 	}
 
 	@Override
-	public void handleLine(String line, BaseDrawHandler drawer, PropertiesConfig propConfig) {
-		String templateValue = line.substring(KEY.length());
-		double height = drawer.textHeight() + SPACE;
-		double width = drawer.textWidth(templateValue) + SPACE;
+	public void handleValue(String value, BaseDrawHandler drawer, PropertiesConfig propConfig) {
+		double height = drawer.textHeight() + UPPER_SPACE + LOWER_SPACE;
+		double width = drawer.textWidth(value) + LEFT_SPACE;
 		Style style = drawer.getCurrentStyle();
 		drawer.setBackgroundColor(ColorOwn.TRANSPARENT);
 		drawer.setLineType(LineType.DASHED);
-		drawer.drawRectangle(propConfig.getGridElementSize().width - width, 0, width, height);
-		drawer.print(templateValue, propConfig.getGridElementSize().width - drawer.getDistanceHorizontalBorderToText(), height-SPACE/2, AlignHorizontal.RIGHT);
+		int elemWidth = propConfig.getGridElementSize().width;
+		drawer.drawLines(new PointDouble(elemWidth - width, 0), new PointDouble(elemWidth - width, height), new PointDouble(elemWidth, height));
+		drawer.print(value, elemWidth - drawer.getDistanceHorizontalBorderToText(), height-LOWER_SPACE, AlignHorizontal.RIGHT);
 		drawer.setStyle(style); // reset style to state before manipulations for drawing the template class
 		propConfig.addToYPos(height);
 	}
-
-	@Override
-	public List<AutocompletionText> getAutocompletionStrings() {
-		return Arrays.asList(new AutocompletionText(KEY + "text", "print template rectangle on top right corner"));
+	
+	public Priority getPriority() {
+		return Priority.MEDIUM;
 	}
 
 }
