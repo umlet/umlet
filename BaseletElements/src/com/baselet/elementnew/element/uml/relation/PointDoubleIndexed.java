24249072b89fc3077188d434d4b4b93844426f72
AFDiaX <AFDiaX@79637776-beb9-4c1a-dc75-bc3ee3110a07>
2014-04-21 17:15:20 +0000

    index rebuild after point merging better readable toString of PointDoubleIndexed

--- BaseletElements/src/com/baselet/elementnew/element/uml/relation/PointDoubleIndexed.java
+++ BaseletElements/src/com/baselet/elementnew/element/uml/relation/PointDoubleIndexed.java
@@ -35,7 +35,7 @@ public class PointDoubleIndexed extends PointDouble {
 
 	@Override
 	public String toString() {
-		return "PointDoubleIndexed [index=" + index + ", x=" + x + ", y=" + y + "]";
+		return "p(i=" + index + " x=" + x + " y=" + y + ")";
 	}
 
 }
