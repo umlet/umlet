ef4becb1c3b14c81e605a160db4ce5a427f67655
AFDiaX <AFDiaX@79637776-beb9-4c1a-dc75-bc3ee3110a07>
2014-04-26 11:45:50 +0000

    NaN handling for xLimit intersection separated (sometimes NaN should have priority over others sometimes not)

--- BaseletElements/src/com/baselet/diagram/draw/geom/XValues.java
+++ BaseletElements/src/com/baselet/diagram/draw/geom/XValues.java
@@ -42,12 +42,26 @@ public class XValues {
 	
 	/**
 	 * returns the intersection of both points [eg: (2,5) intersect (1,4) = (2,4)]
+	 * @param nanPriority if true then NaN has priority over other values, otherwise other values have priority
 	 */
-	public XValues intersect(XValues other) {
-		double maxLeft = left;
-		double minRight = right;
-		if (!other.left.equals(Double.NaN) && other.left > this.left) maxLeft = other.left;
-		if (!other.right.equals(Double.NaN) && other.right < this.right) minRight = other.right;
+	public XValues intersect(XValues other, boolean nanPriority) {
+		Double maxLeft = left;
+		Double minRight = right;
+		if (nanPriority) {
+			if (other.left.equals(Double.NaN) || other.left > this.left) {
+				maxLeft = other.left;
+			}
+			if (other.right.equals(Double.NaN) || other.right < this.right) {
+				minRight = other.right;
+			}
+		} else {
+			if (left.equals(Double.NaN) || other.left > this.left) {
+				maxLeft = other.left;
+			}
+			if (right.equals(Double.NaN) || other.right < this.right) {
+				minRight = other.right;
+			}
+		}
 		return new XValues(maxLeft, minRight);
 	}
 
