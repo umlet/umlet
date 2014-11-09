ba4836e9d2dafc9d12461ddfeb1c3112ed3a894e
AFDiaX <AFDiaX@79637776-beb9-4c1a-dc75-bc3ee3110a07>
2014-11-09 09:41:52 +0000

    dependency on Relation changed to Stickable

--- trunk/BaseletElements/src/com/baselet/diagram/Diagram.java
+++ trunk/BaseletElements/src/com/baselet/diagram/Diagram.java
@@ -5,12 +5,12 @@ import java.util.List;
 
 import com.baselet.element.GridElement;
 import com.baselet.element.HasPanelAttributes;
+import com.baselet.element.sticking.Stickable;
 import com.baselet.element.sticking.StickableMap;
-import com.baselet.elementnew.element.uml.relation.Relation;
 
 public interface Diagram extends HasPanelAttributes, HasGridElements {
 
-	public abstract List<Relation> getRelations();
+	public abstract List<Stickable> getStickables();
 
 	public abstract StickableMap getStickables(GridElement draggedElement);
 
