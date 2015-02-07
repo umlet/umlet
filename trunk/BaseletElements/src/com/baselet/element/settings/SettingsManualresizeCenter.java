dd505a970d670a2bf2131f9cdda0ccbffb552ba7
AFDiaX <AFDiaX@79637776-beb9-4c1a-dc75-bc3ee3110a07>
2015-02-07 18:55:14 +0000

    refactoring settings

--- trunk/BaseletElements/src/com/baselet/element/settings/SettingsManualresizeCenter.java
+++ trunk/BaseletElements/src/com/baselet/element/settings/SettingsManualresizeCenter.java
@@ -2,28 +2,12 @@ package com.baselet.element.settings;
 
 import java.util.List;
 
-import com.baselet.control.basics.XValues;
-import com.baselet.control.enums.AlignHorizontal;
 import com.baselet.control.enums.AlignVertical;
 import com.baselet.element.facet.ElementStyleEnum;
 import com.baselet.element.facet.Facet;
 import com.baselet.element.facet.Settings;
 
 public abstract class SettingsManualresizeCenter extends Settings {
-	@Override
-	public XValues getXValues(double y, int height, int width) {
-		return new XValues(0, width);
-	}
-
-	@Override
-	public AlignVertical getVAlign() {
-		return AlignVertical.CENTER;
-	}
-
-	@Override
-	public AlignHorizontal getHAlign() {
-		return AlignHorizontal.CENTER;
-	}
 
 	@Override
 	public ElementStyleEnum getElementStyle() {
@@ -32,6 +16,12 @@ public abstract class SettingsManualresizeCenter extends Settings {
 
 	@Override
 	protected List<Facet> createFacets() {
-		return Settings.ALL;
+		return Settings.MANUALRESIZE;
 	}
+
+	@Override
+	public AlignVertical getVAlign() {
+		return AlignVertical.CENTER;
+	}
+
 }
--- trunk/BaseletElements/src/com/baselet/element/settings/SettingsText.java
+++ trunk/BaseletElements/src/com/baselet/element/settings/SettingsText.java
@@ -2,29 +2,13 @@ package com.baselet.element.settings;
 
 import java.util.List;
 
-import com.baselet.control.basics.XValues;
 import com.baselet.control.enums.AlignHorizontal;
-import com.baselet.control.enums.AlignVertical;
 import com.baselet.element.facet.ElementStyleEnum;
 import com.baselet.element.facet.Facet;
 import com.baselet.element.facet.Settings;
 import com.baselet.element.facet.common.SeparatorLineFacet;
 
 public class SettingsText extends Settings {
-	@Override
-	public XValues getXValues(double y, int height, int width) {
-		return new XValues(0, width);
-	}
-
-	@Override
-	public AlignVertical getVAlign() {
-		return AlignVertical.TOP;
-	}
-
-	@Override
-	public AlignHorizontal getHAlign() {
-		return AlignHorizontal.LEFT;
-	}
 
 	@Override
 	public ElementStyleEnum getElementStyle() {
@@ -33,6 +17,12 @@ public class SettingsText extends Settings {
 
 	@Override
 	protected List<Facet> createFacets() {
-		return listOf(Settings.ALL, SeparatorLineFacet.INSTANCE);
+		return listOf(Settings.MANUALRESIZE, SeparatorLineFacet.INSTANCE);
 	}
+
+	@Override
+	public AlignHorizontal getHAlign() {
+		return AlignHorizontal.LEFT;
+	}
+
 }
