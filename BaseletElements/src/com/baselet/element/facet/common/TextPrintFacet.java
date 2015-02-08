ce5ca4a6dffa84303feabe961bb30c1c0a108914
AFDiaX <AFDiaX@79637776-beb9-4c1a-dc75-bc3ee3110a07>
2015-02-08 10:44:41 +0000

    ypos calc moed to TextPrintFacet

--- BaseletElements/src/com/baselet/element/facet/common/TextPrintFacet.java
+++ BaseletElements/src/com/baselet/element/facet/common/TextPrintFacet.java
@@ -5,8 +5,10 @@ import java.util.List;
 
 import com.baselet.control.basics.XValues;
 import com.baselet.control.enums.AlignHorizontal;
+import com.baselet.control.enums.ElementStyle;
 import com.baselet.control.enums.Priority;
 import com.baselet.diagram.draw.DrawHandler;
+import com.baselet.diagram.draw.TextSplitter;
 import com.baselet.element.facet.Facet;
 import com.baselet.element.facet.PropertiesParserState;
 import com.baselet.gui.AutocompletionText;
@@ -24,6 +26,8 @@ public class TextPrintFacet extends Facet {
 
 	@Override
 	public void handleLine(String line, DrawHandler drawer, PropertiesParserState state) {
+		setupAtFirstLine(line, drawer, state);
+
 		XValues xLimitsForText = state.getXLimitsForArea(state.getyPos(), drawer.textHeightMax(), false);
 		Double spaceNotUsedForText = state.getGridElementSize().width - xLimitsForText.getSpace();
 		if (!spaceNotUsedForText.equals(Double.NaN)) { // NaN is possible if xlimits calculation contains e.g. a division by zero
@@ -32,6 +36,45 @@ public class TextPrintFacet extends Facet {
 		AlignHorizontal hAlign = state.gethAlign();
 		drawer.print(line, calcHorizontalTextBoundaries(xLimitsForText, drawer.getDistanceBorderToText(), hAlign), state.getyPos(), hAlign);
 		state.addToYPos(drawer.textHeightMaxWithSpace());
+	}
+
+	/**
+	 * before the first line is printed, some space-setup is necessary to make sure the text position is correct
+	 */
+	private static void setupAtFirstLine(String line, DrawHandler drawer, PropertiesParserState state) {
+		boolean isFirstPrintedLine = state.getFacetResponse(TextPrintFacet.class, true);
+		if (isFirstPrintedLine) {
+			calcTopDisplacementToFitLine(line, state, drawer);
+			state.setFacetResponse(TextPrintFacet.class, false);
+		}
+	}
+
+	/**
+	 * Calculates the necessary y-pos space to make the first line fit the xLimits of the element
+	 * Currently only used by UseCase element to make sure the first line is moved down as long as it doesn't fit into the available space
+	 */
+	private static void calcTopDisplacementToFitLine(String firstLine, PropertiesParserState state, DrawHandler drawer) {
+		boolean wordwrap = state.getElementStyle() == ElementStyle.WORDWRAP;
+		if (!wordwrap) { // in case of wordwrap or no text, there is no top displacement
+			int BUFFER = 2; // a small buffer between text and outer border
+			double textHeight = drawer.textHeightMax();
+			double addedSpacePerIteration = textHeight / 2;
+			double yPosToCheck = state.getyPos();
+			double availableWidthSpace = state.getXLimitsForArea(yPosToCheck, textHeight, true).getSpace() - BUFFER;
+			int maxLoops = 1000;
+			while (yPosToCheck < state.getGridElementSize().height && !TextSplitter.checkifStringFits(firstLine, availableWidthSpace, drawer)) {
+				if (maxLoops-- < 0) {
+					throw new RuntimeException("Endless loop during calculation of top displacement");
+				}
+				yPosToCheck += addedSpacePerIteration;
+				double previousWidthSpace = availableWidthSpace;
+				availableWidthSpace = state.getXLimitsForArea(yPosToCheck, textHeight, true).getSpace() - BUFFER;
+				// only set displacement if the last iteration resulted in a space gain (eg: for UseCase until the middle, for Class: stays on top because on a rectangle there is never a width-space gain)
+				if (availableWidthSpace > previousWidthSpace) {
+					state.addToYPos(addedSpacePerIteration);
+				}
+			}
+		}
 	}
 
 	private static double calcHorizontalTextBoundaries(XValues xLimitsForText, double distanceBorderToText, AlignHorizontal hAlign) {
