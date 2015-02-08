5e5f279ca83d7a2a38afa114ffb5d7663812bebd
AFDiaX <AFDiaX@79637776-beb9-4c1a-dc75-bc3ee3110a07>
2015-02-08 10:31:57 +0000

    refactoring top displacement

--- BaseletElements/src/com/baselet/element/PropertiesParserUtils.java
+++ BaseletElements/src/com/baselet/element/PropertiesParserUtils.java
@@ -1,7 +1,5 @@
 package com.baselet.element;
 
-import java.util.List;
-
 import com.baselet.control.enums.ElementStyle;
 import com.baselet.diagram.draw.DrawHandler;
 import com.baselet.diagram.draw.TextSplitter;
@@ -9,26 +7,30 @@ import com.baselet.element.facet.PropertiesParserState;
 
 public class PropertiesParserUtils {
 
-	static double calcTopDisplacementToFitLine(List<String> propertiesText, double startPoint, PropertiesParserState state, DrawHandler drawer) {
-		double displacement = startPoint;
+	/**
+	 * Calculates the necessary y-pos space to make the first line fit the xLimits of the element
+	 * Currently only used by UseCase element to make sure the first line is moved down as long as it doesn't fit into the available space
+	 */
+	static double calcTopDisplacementToFitLine(String firstLine, PropertiesParserState state, DrawHandler drawer) {
+		double displacement = 0;
 		boolean wordwrap = state.getElementStyle() == ElementStyle.WORDWRAP;
-		if (!wordwrap && !propertiesText.isEmpty()) { // in case of wordwrap or no text, there is no top displacement
+		if (!wordwrap) { // in case of wordwrap or no text, there is no top displacement
 			int BUFFER = 2; // a small buffer between text and outer border
 			double textHeight = drawer.textHeightMax();
-			String firstLine = propertiesText.iterator().next();
-			double availableWidthSpace = state.getXLimitsForArea(displacement, textHeight, true).getSpace() - BUFFER;
-			double accumulator = displacement;
+			double addedSpacePerIteration = textHeight / 2;
+			double yPosToCheck = state.getyPos();
+			double availableWidthSpace = state.getXLimitsForArea(yPosToCheck, textHeight, true).getSpace() - BUFFER;
 			int maxLoops = 1000;
-			while (accumulator < state.getGridElementSize().height && !TextSplitter.checkifStringFits(firstLine, availableWidthSpace, drawer)) {
+			while (yPosToCheck < state.getGridElementSize().height && !TextSplitter.checkifStringFits(firstLine, availableWidthSpace, drawer)) {
 				if (maxLoops-- < 0) {
 					throw new RuntimeException("Endless loop during calculation of top displacement");
 				}
-				accumulator += textHeight / 2;
+				yPosToCheck += addedSpacePerIteration;
 				double previousWidthSpace = availableWidthSpace;
-				availableWidthSpace = state.getXLimitsForArea(accumulator, textHeight, true).getSpace() - BUFFER;
+				availableWidthSpace = state.getXLimitsForArea(yPosToCheck, textHeight, true).getSpace() - BUFFER;
 				// only set displacement if the last iteration resulted in a space gain (eg: for UseCase until the middle, for Class: stays on top because on a rectangle there is never a width-space gain)
 				if (availableWidthSpace > previousWidthSpace) {
-					displacement = accumulator;
+					displacement += addedSpacePerIteration;
 				}
 			}
 		}
