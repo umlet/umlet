519249359af64055dfff3f41901b6043efdc3819
AFDiaX <AFDiaX@79637776-beb9-4c1a-dc75-bc3ee3110a07>
2015-02-08 11:26:12 +0000

    javadoc

--- BaseletElements/src/com/baselet/element/facet/GlobalFacet.java
+++ BaseletElements/src/com/baselet/element/facet/GlobalFacet.java
@@ -2,7 +2,7 @@ package com.baselet.element.facet;
 
 /**
  * Global facets are parsed before any other ones, because they influence the whole diagram, even if they are located at the bottom
- * e.g. style=wordwrap may be located at the bottom but has an influence on every printed line, style=autoresize is even more important because it influences the size of the whole element
+ * e.g. bg=red must be known before drawing the common content of an element; style=autoresize must be known as soon as possible to make the size-calculations
  */
 public abstract class GlobalFacet extends Facet {
 
