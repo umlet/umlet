08b38466e13485c010fc09e7902a4243d227acbe
AFDiaX <AFDiaX@79637776-beb9-4c1a-dc75-bc3ee3110a07>
2015-02-15 18:25:23 +0000

    removed java implementation enum

--- Baselet/src/com/baselet/control/constants/SystemInfo.java
+++ Baselet/src/com/baselet/control/constants/SystemInfo.java
@@ -1,13 +1,11 @@
 package com.baselet.control.constants;
 
-import com.baselet.control.enums.JavaImplementation;
 import com.baselet.control.enums.Metakey;
 import com.baselet.control.enums.Os;
 
 public class SystemInfo {
 
 	public static final Os OS;
-	public static final JavaImplementation JAVA_IMPL;
 	public static final String JAVA_VERSION = java.lang.System.getProperty("java.specification.version");
 	public static final Metakey META_KEY;
 
@@ -27,13 +25,6 @@ public class SystemInfo {
 		}
 		else {
 			OS = Os.UNKNOWN;
-		}
-
-		if (java.lang.System.getProperty("java.runtime.name").toUpperCase().contains("OPEN")) {
-			JAVA_IMPL = JavaImplementation.OPEN;
-		}
-		else {
-			JAVA_IMPL = JavaImplementation.SUN;
 		}
 
 		if (SystemInfo.OS == Os.MAC) {
--- Baselet/src/com/baselet/control/enums/JavaImplementation.java
+++
@@ -1,5 +0,0 @@
-package com.baselet.control.enums;
-
-public enum JavaImplementation {
-	OPEN, SUN
-}
