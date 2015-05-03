46f7805973e5816f5fb04cbf99059115f2b319ef
afdia <Dia@Dia-PC>
2015-05-03 21:30:20 +0200

    #260: relation selection is not changed if multiple relations (which are on the same layer) overlap

--- Baselet/src/com/baselet/element/ElementUtils.java
+++ Baselet/src/com/baselet/element/ElementUtils.java
@@ -7,8 +7,9 @@ import javax.swing.JComponent;
 import com.baselet.control.Main;
 import com.baselet.control.basics.Converter;
 import com.baselet.control.basics.geom.Point;
+import com.baselet.diagram.DrawPanel;
 import com.baselet.element.interfaces.GridElement;
-import com.baselet.element.old.element.Relation;
+import com.baselet.element.relation.Relation;
 
 public class ElementUtils {
 
@@ -16,6 +17,9 @@ public class ElementUtils {
 	 * Must be overwritten because Swing uses this method to tell if 2 elements are overlapping
 	 * It's also used to determine which element gets selected if there are overlapping elements (the smallest one)
 	 * IMPORTANT: on overlapping elements, contains is called for all elements until the first one returns true, then the others contain methods are not called
+	 *
+	 * In future this logic should be the same as in BaseletGWT.DrawPanel.getGridElementOnPosition() which is much simpler because its externally calculated
+	 * (this migration is only possible if there are not GridElement Listeners, but instead only one Diagram Listener which delegates the event to the specific GridElement like in GWT)
 	 */
 	public static boolean checkForOverlap(GridElement gridElement, Point p) {
 		JComponent component = (JComponent) gridElement.getComponent();
@@ -25,15 +29,22 @@ public class ElementUtils {
 			return false;
 		}
 
-		List<GridElement> elements = Main.getHandlerForElement(gridElement).getDrawPanel().getGridElements();
-		// Selector selector = drawPanel.getSelector();
+		DrawPanel drawPanel = Main.getHandlerForElement(gridElement).getDrawPanel();
+		List<GridElement> elements = drawPanel.getGridElements();
+		Selector selector = drawPanel.getSelector();
 		for (GridElement other : elements) {
-			if (other == gridElement) {
-				continue;
+			if (other == gridElement || other.getLayer() < gridElement.getLayer() || !other.isSelectableOn(absolute)) {
+				continue; // ignore this element, elements with a lower layer and elements which are not selectable on the point
 			}
-			if (other.getLayer() < gridElement.getLayer())
-			{
-				continue; // elements with lower layer are ignored
+
+			// issue #260: if a relation is the only selected element its selection should be retained
+			if (gridElement instanceof Relation && other instanceof Relation) {
+				if (selector.isSelectedOnly(gridElement)) {
+					return true;
+				}
+				if (selector.isSelectedOnly(other)) {
+					return false;
+				}
 			}
 
 			JComponent otherComponent = (JComponent) other.getComponent();
@@ -50,17 +61,17 @@ public class ElementUtils {
 			other_rectangle.x += other.getRectangle().x - gridElement.getRectangle().x;
 			other_rectangle.y += other.getRectangle().y - gridElement.getRectangle().y;
 			// when elements intersect, select the smaller element except if it is an old relation (because they have a larger rectangle than they use). NOTE: Old Relations are not checked because they do not properly implement isSelectableOn
-			if (!(other instanceof Relation) && other.isSelectableOn(absolute) && rectangle.intersects(other_rectangle) && ElementUtils.smaller(other_rectangle, rectangle)) {
+			if (!(other instanceof com.baselet.element.old.element.Relation) && rectangle.intersects(other_rectangle) && firstSmallerThanSecond(other_rectangle, rectangle)) {
 				return false;
 			}
 		}
 		return true;
 	}
 
-	private static boolean smaller(java.awt.Rectangle a, java.awt.Rectangle b) {
-		int areaA = a.getSize().height * a.getSize().width;
-		int areaB = b.getSize().height * b.getSize().width;
-		if (areaA < areaB) {
+	private static boolean firstSmallerThanSecond(java.awt.Rectangle first, java.awt.Rectangle second) {
+		int areaFirst = first.getSize().height * first.getSize().width;
+		int areaSecond = second.getSize().height * second.getSize().width;
+		if (areaFirst < areaSecond) {
 			return true;
 		}
 		return false;
