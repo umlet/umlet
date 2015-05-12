2990fd0b482e83bdf2d310cbe6c66873ae956a7d
afdia <Dia@Dia-PC>
2015-05-12 08:46:50 +0200

    #206: IME works as expected

--- Baselet/src/com/baselet/gui/pane/OwnSyntaxPane.java
+++ Baselet/src/com/baselet/gui/pane/OwnSyntaxPane.java
@@ -154,7 +154,9 @@ public class OwnSyntaxPane {
 	}
 
 	private void setText(String text) {
-		textArea.setText(text); // Always set text even if they are equal to trigger correct syntax highlighting (if words to highlight have changed but text not)
+		if (!textArea.getText().equals(text)) {
+			textArea.setText(text); // Always set text even if they are equal to trigger correct syntax highlighting (if words to highlight have changed but text not)
+		}
 		textArea.setCaretPosition(0);
 		createHightLightMap();
 		createAutocompletionCompletionProvider();
