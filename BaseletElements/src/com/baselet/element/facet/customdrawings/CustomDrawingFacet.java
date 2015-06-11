3d0c82ba12330c4917ffd3634f98be0bc29a68d5
Raimund Hirz <hirzraimund@users.noreply.github.com>
2015-06-11 22:19:56 +0200

    improved code completion for custom drawing commandsadded own exception which encapsulates the generated exception

--- BaseletElements/src/com/baselet/element/facet/customdrawings/CustomDrawingFacet.java
+++ BaseletElements/src/com/baselet/element/facet/customdrawings/CustomDrawingFacet.java
@@ -9,7 +9,6 @@ import com.baselet.control.enums.FormatLabels;
 import com.baselet.control.enums.Priority;
 import com.baselet.element.facet.Facet;
 import com.baselet.element.facet.PropertiesParserState;
-import com.baselet.element.facet.customdrawings.gen.ParseException;
 import com.baselet.gui.AutocompletionText;
 
 public class CustomDrawingFacet extends Facet {
@@ -45,12 +44,7 @@ public class CustomDrawingFacet extends Facet {
 		}
 		else {
 			state.getGridElementSize().getWidth();
-			try {
-				new CustomDrawingParserImpl(line, state.getGridElementSize().getWidth(), state.getGridElementSize().getHeight(), state.getDrawer()).parse();
-			} catch (ParseException e) {
-				// remove the trailing \n from the error message
-				throw new RuntimeException(e.getMessage().substring(0, e.getMessage().length() - 1));
-			}
+			CustomDrawingParserImpl.parse(line, state.getGridElementSize().getWidth(), state.getGridElementSize().getHeight(), state.getDrawer());
 		}
 	}
 
@@ -59,13 +53,26 @@ public class CustomDrawingFacet extends Facet {
 		List<AutocompletionText> autocompletionList = new LinkedList<AutocompletionText>();
 		autocompletionList.add(new AutocompletionText(CODE_SEP_START, CODE_START_INFO));
 
-		autocompletionList.add(new AutocompletionText("drawLine(x1, y1, x2, y2)", "Draws a line from (x1, y1) to (x2, y2). Supports fg, lt and lw option after the last bracket."));
-		autocompletionList.add(new AutocompletionText("drawRectangle(x, y, width, height)", "Draws a rectangle with the top left corner at (x, y). Supports fg, bg, lt and lw option after the last bracket."));
-		autocompletionList.add(new AutocompletionText("drawRectangleRound(x, y, width, height, radius)", "Draws a rectangle with round corners with the top left corner at (x, y). Supports fg, bg, lt and lw option after the last bracket."));
-		autocompletionList.add(new AutocompletionText("drawCircle(x, y, radius)", "Draws a circle with the center at (x, y) and the given radius. Supports fg, bg, lt and lw option after the last bracket."));
-		autocompletionList.add(new AutocompletionText("drawEllipse(x, y, width, height)", "Draws an ellipse where the top left corner of the surrounding rectangle is at (x, y). Supports fg, bg, lt and lw option after the last bracket."));
-		autocompletionList.add(new AutocompletionText("drawArc(x, y, width, height, start, extent, open)", "Draws an elliptical arc where the top left corner of the surrounding rectangle is at (x, y). This arc starts at <start> degrees and ends at <extent> degrees. If open is false 2 lines are drawn from the center to the arc. Supports fg, bg, lt and lw option after the last bracket."));
-		autocompletionList.add(new AutocompletionText("drawText(\"This is text!\", x, y, CENTER)", "Draws the text at (x, y) with the given horizontal alignment. Supports fg option after the last bracket."));
+		autocompletionList.add(new AutocompletionText("drawLine(20, 10, 50, 20) //fg= lt= lw= //Parameters (x1, y1, x2, y2) ",
+				"Draws a line from (x1, y1) to (x2, y2). Supports fg, lt and lw option after the last bracket."));
+
+		autocompletionList.add(new AutocompletionText("drawRectangle(5,5,40,20) //fg= bg= lt= lw= //Parameters (x, y, width, height)",
+				"Draws a rectangle with the top left corner at (x, y). Supports fg, bg, lt and lw option after the last bracket."));
+
+		autocompletionList.add(new AutocompletionText("drawRectangleRound(5,5,40,20,5) //fg= bg= lt= lw= //Parameters (x, y, width, height, radius)",
+				"Draws a rectangle with round corners with the top left corner at (x, y). Supports fg, bg, lt and lw option after the last bracket."));
+
+		autocompletionList.add(new AutocompletionText("drawCircle(20,20,20) //fg= bg= lt= lw= //Parameters (x, y, radius)",
+				"Draws a circle with the center at (x, y) and the given radius. Supports fg, bg, lt and lw option after the last bracket."));
+
+		autocompletionList.add(new AutocompletionText("drawEllipse(0,0,40,20) //fg= bg= lt= lw= //Parameters (x, y, width, height)",
+				"Draws an ellipse where the top left corner of the surrounding rectangle is at (x, y). Supports fg, bg, lt and lw option after the last bracket."));
+
+		autocompletionList.add(new AutocompletionText("drawArc(0,0,60,30,0,270,false) //fg= bg= lt= lw= //Parameters (x, y, width, height, start, extent, open)",
+				"Draws an elliptical arc where the top left corner of the surrounding rectangle is at (x, y). This arc starts at <start> degrees and ends at <start>+<extent> degrees. If open is false 2 lines are drawn from the center to the arc. Supports fg, bg, lt and lw option after the last bracket."));
+
+		autocompletionList.add(new AutocompletionText("drawText(\"This is text!\", width/2, height/2, CENTER) //fg= //Parameters (text, x, y, horizontal alignment)",
+				"Draws the text at (x, y) with the given horizontal alignment. Supports fg option after the last bracket."));
 
 		return autocompletionList;
 	}
--- BaseletElements/src/com/baselet/element/facet/customdrawings/CustomDrawingParserImpl.java
+++ BaseletElements/src/com/baselet/element/facet/customdrawings/CustomDrawingParserImpl.java
@@ -5,6 +5,7 @@ import com.baselet.element.facet.customdrawings.gen.CustomDrawingParser;
 import com.baselet.element.facet.customdrawings.gen.CustomDrawingParserTokenManager;
 import com.baselet.element.facet.customdrawings.gen.ParseException;
 import com.baselet.element.facet.customdrawings.gen.Provider;
+import com.baselet.element.facet.customdrawings.gen.StringProvider;
 import com.baselet.element.facet.customdrawings.gen.TokenMgrException;
 
 public class CustomDrawingParserImpl extends CustomDrawingParser {
@@ -20,11 +21,8 @@ public class CustomDrawingParserImpl extends CustomDrawingParser {
 		this.drawHandler = drawHandler;
 	}
 
-	public CustomDrawingParserImpl(String dsl, double width, double height, DrawHandler drawHandler) throws ParseException, TokenMgrException {
-		super(dsl);
-		this.width = width;
-		this.height = height;
-		this.drawHandler = drawHandler;
+	public CustomDrawingParserImpl(String dsl, double width, double height, DrawHandler drawHandler) {
+		this(new StringProvider(dsl), width, height, drawHandler);
 	}
 
 	public CustomDrawingParserImpl(Provider stream, double width, double height, DrawHandler drawHandler) {
@@ -32,6 +30,60 @@ public class CustomDrawingParserImpl extends CustomDrawingParser {
 		this.width = width;
 		this.height = height;
 		this.drawHandler = drawHandler;
+	}
+
+	/**
+	 *
+	 * @param line String which should be parsed
+	 * @param width of the element, can be used in the drawing command
+	 * @param height of the element, can be used in the drawing command
+	 * @param drawer is used to execute the drawing commands
+	 * @throws CustomDrawingParserRuntimeException if the input couldn't be parsed (cause is a ParseException or TokenMgrException)
+	 */
+	public static void parse(String line, int width, int height, DrawHandler drawer) {
+		new CustomDrawingParserImpl(line, width, height, drawer).parse();
+	}
+
+	/**
+	 * calls the function for the start symbol and catches exceptions which are re-thrown as CustomDrawingParserRuntimeException
+	 * @throws CustomDrawingParserRuntimeException if the input couldn't be parsed (cause is a ParseException or TokenMgrException)
+	 * @see CustomDrawingParserImpl#start()
+	 */
+	public void parse() {
+		try {
+			start();
+		} catch (ParseException e) {
+			throw new CustomDrawingParserRuntimeException(e);
+		} catch (TokenMgrException e) {
+			throw new CustomDrawingParserRuntimeException(e);
+		}
+	}
+
+	/**
+	 *
+	 * @param line String which should be parsed
+	 * @param width of the element, can be used in the drawing command
+	 * @param height of the element, can be used in the drawing command
+	 * @param drawer is used to execute the drawing commands
+	 * @throws CustomDrawingParserException if the input couldn't be parsed (cause is a ParseException or TokenMgrException)
+	 */
+	public static void parseCheckedEx(String line, int width, int height, DrawHandler drawer) throws CustomDrawingParserException {
+		new CustomDrawingParserImpl(line, width, height, drawer).parse();
+	}
+
+	/**
+	 * calls the function for the start symbol and catches exceptions which are re-thrown as CustomDrawingParserException
+	 * @throws CustomDrawingParserException if the input couldn't be parsed (cause is a ParseException or TokenMgrException)
+	 * @see CustomDrawingParserImpl#start()
+	 */
+	public void parseCheckedEx() throws CustomDrawingParserException {
+		try {
+			start();
+		} catch (ParseException e) {
+			throw new CustomDrawingParserException(e);
+		} catch (TokenMgrException e) {
+			throw new CustomDrawingParserException(e);
+		}
 	}
 
 	@Override
@@ -48,5 +100,4 @@ public class CustomDrawingParserImpl extends CustomDrawingParser {
 	public DrawHandler getDrawHandler() {
 		return drawHandler;
 	}
-
 }
