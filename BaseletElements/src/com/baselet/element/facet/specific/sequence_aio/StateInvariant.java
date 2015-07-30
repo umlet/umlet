2078f0f517ea1eccd44f7dac8b8047f8f6d4bbf5
Raimund Hirz <hirzraimund@users.noreply.github.com>
2015-07-30 12:23:57 +0200

    changed the StateInvariant code so it uses the new AdvancedTextSplitter, in this process some minor drawing bugs were fixed

--- BaseletElements/src/com/baselet/element/facet/specific/sequence_aio/StateInvariant.java
+++ BaseletElements/src/com/baselet/element/facet/specific/sequence_aio/StateInvariant.java
@@ -1,20 +1,20 @@
 package com.baselet.element.facet.specific.sequence_aio;
 
-import java.util.ArrayList;
-import java.util.Collection;
-
 import com.baselet.control.basics.Line1D;
 import com.baselet.control.basics.geom.PointDouble;
 import com.baselet.control.enums.AlignHorizontal;
+import com.baselet.control.enums.AlignVertical;
+import com.baselet.diagram.draw.AdvancedTextSplitter;
 import com.baselet.diagram.draw.DrawHandler;
-import com.baselet.diagram.draw.TextSplitter;
 
-// TODO change to the advancedtextsplitter
 public class StateInvariant implements LifelineOccurrence {
 	/** the width of the ellipse which builds the left and right border (no text is drawn in this) */
 	private static final double ROUND_PART_WIDTH = 20;
+	/** how much of the round part width can be used for drawing text */
+	private static final double ROUND_PART_TEXT_PERCENTAGE = 0.3;
 	/** the space between the text and the border line */
 	private static final double VERTICAL_BORDER_PADDING = 5;
+	private static final double CURLY_BRACKETS_Y_PADDING = 2;
 	/** the minimum width, to avoid splitting text with very short words e.g. print "I am I" in one line instead of 3*/
 	private static final double MIN_WIDTH = 50;
 
@@ -39,22 +39,21 @@ public class StateInvariant implements LifelineOccurrence {
 
 	@Override
 	public Line1D draw(DrawHandler drawHandler, PointDouble topLeft, PointDouble size) {
-		Collection<String> wrappedText = splitText(drawHandler, size.x);
-		double height = getHeight(drawHandler, size.x, wrappedText);
-		double textY = topLeft.y + (size.y - height) / 2;
-		Line1D interruptedLifeline = new Line1D(textY, textY + height);
+		double height = getHeight(drawHandler, size.x);
+		double topY = topLeft.y + (size.y - height) / 2;
+		Line1D interruptedLifeline = new Line1D(topY, topY + height);
 		if (style == StateInvariantStyle.STATE) {
 			// draw border and adjust textY and textLeftX
-			drawHandler.drawArc(topLeft.x, textY, ROUND_PART_WIDTH * 2, height, 90, 180, true);
-			drawHandler.drawArc(topLeft.x + size.x - ROUND_PART_WIDTH * 2, textY, ROUND_PART_WIDTH * 2, height, 270, 180, true);
-			drawHandler.drawLine(topLeft.x + ROUND_PART_WIDTH, textY, topLeft.x + size.x - ROUND_PART_WIDTH, textY);
-			drawHandler.drawLine(topLeft.x + ROUND_PART_WIDTH, textY + height, topLeft.x + size.x - ROUND_PART_WIDTH, textY + height);
-			textY += VERTICAL_BORDER_PADDING;
+			drawHandler.drawArc(topLeft.x, topY, ROUND_PART_WIDTH * 2, height, 90, 180, true);
+			drawHandler.drawArc(topLeft.x + size.x - ROUND_PART_WIDTH * 2, topY, ROUND_PART_WIDTH * 2, height, 270, 180, true);
+			drawHandler.drawLine(topLeft.x + ROUND_PART_WIDTH, topY, topLeft.x + size.x - ROUND_PART_WIDTH, topY);
+			drawHandler.drawLine(topLeft.x + ROUND_PART_WIDTH, topY + height, topLeft.x + size.x - ROUND_PART_WIDTH, topY + height);
+			AdvancedTextSplitter.drawText(drawHandler, lines, topLeft.x + ROUND_PART_WIDTH * (1 - ROUND_PART_TEXT_PERCENTAGE), topY,
+					size.x - ROUND_PART_WIDTH * (1 - ROUND_PART_TEXT_PERCENTAGE) * 2, height, AlignHorizontal.CENTER, AlignVertical.CENTER);
 		}
-		textY += drawHandler.textHeightMax();
-		for (String line : wrappedText) {
-			drawHandler.print(line, topLeft.x + size.x / 2.0, textY, AlignHorizontal.CENTER);
-			textY += drawHandler.textHeightMaxWithSpace();
+		else if (style == StateInvariantStyle.CURLY_BRACKETS) {
+			AdvancedTextSplitter.drawText(drawHandler, lines, topLeft.x, topY, size.x, height,
+					AlignHorizontal.CENTER, AlignVertical.CENTER);
 		}
 		return interruptedLifeline;
 
@@ -62,18 +61,12 @@ public class StateInvariant implements LifelineOccurrence {
 
 	@Override
 	public double getMinWidth(DrawHandler drawHandler) {
-		// search for the biggest word which can't be wrapped and return its size
-		double minWidth = MIN_WIDTH;
-		for (String line : lines) {
-			for (String word : line.split("\\s+")) {
-				minWidth = Math.max(minWidth, drawHandler.textWidth(word));
-			}
-		}
+		double minWidth = Math.max(MIN_WIDTH, AdvancedTextSplitter.getTextMinWidth(lines, drawHandler));
 		if (style == StateInvariantStyle.CURLY_BRACKETS) {
 			return minWidth;
 		}
 		else {
-			return minWidth + ROUND_PART_WIDTH * 2;
+			return minWidth + ROUND_PART_WIDTH * (1 - ROUND_PART_TEXT_PERCENTAGE) * 2;
 		}
 	}
 
@@ -82,43 +75,16 @@ public class StateInvariant implements LifelineOccurrence {
 		return getHeight(drawHandler, size.x) - size.y;
 	}
 
-	/**
-	 * calculate the needed y space in respect to the width which we can use
-	 * @param drawHandler
-	 * @param textWidth
-	 * @return the needed y space in respect to the width which we can use
-	 */
-	private double getHeight(DrawHandler drawHandler, double textWidth) {
-		return getHeight(drawHandler, textWidth, splitText(drawHandler, textWidth));
-	}
-
-	/**
-	 * calculate the needed y space in respect to the width which we can use
-	 * @param drawHandler
-	 * @param textWidth
-	 * @param wrappedText the already wrapped text
-	 * @return the needed y space in respect to the width which we can use
-	 */
-	private double getHeight(DrawHandler drawHandler, double textWidth, Collection<String> wrappedText) {
+	private double getHeight(DrawHandler drawHandler, double totalWidth) {
+		if (style == StateInvariantStyle.STATE) {
+			totalWidth -= ROUND_PART_WIDTH * (1 - ROUND_PART_TEXT_PERCENTAGE) * 2;
+		}
 		if (style == StateInvariantStyle.CURLY_BRACKETS) {
-			return wrappedText.size() * drawHandler.textHeightMaxWithSpace();
+			return AdvancedTextSplitter.getSplitStringHeight(lines, totalWidth, drawHandler) + CURLY_BRACKETS_Y_PADDING * 2;
 		}
 		else {
-			return wrappedText.size() * drawHandler.textHeightMaxWithSpace() + VERTICAL_BORDER_PADDING * 2;
+			return AdvancedTextSplitter.getSplitStringHeight(lines, totalWidth, drawHandler) + VERTICAL_BORDER_PADDING * 2;
 		}
-	}
-
-	private Collection<String> splitText(DrawHandler drawHandler, double textWidth) {
-		String wrappedLine;
-		ArrayList<String> splittedLines = new ArrayList<String>(lines.length);
-		for (String line : lines) {
-			do {
-				wrappedLine = TextSplitter.splitString(line, textWidth, drawHandler);
-				splittedLines.add(wrappedLine);
-				line = line.substring(wrappedLine.length()).trim();
-			} while (!line.isEmpty());
-		}
-		return splittedLines;
 	}
 
 	public enum StateInvariantStyle {
