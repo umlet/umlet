3543a2d28676f397ac8e37e3c422267e2984b992
Tekbert <megabite@web.de>
2020-05-19 12:32:52 +0200

    Fixed issue where VS Code exported PNGs would result in the actual editor view being rendered at a different size

--- umlet-gwt/src/main/java/com/baselet/gwt/client/view/widgets/DownloadPopupPanel.java
+++ umlet-gwt/src/main/java/com/baselet/gwt/client/view/widgets/DownloadPopupPanel.java
@@ -3,6 +3,8 @@ package com.baselet.gwt.client.view.widgets;
 import java.util.Date;
 
 import com.baselet.control.enums.Program;
+import com.baselet.element.interfaces.Diagram;
+import com.baselet.gwt.client.view.CanvasUtils;
 import com.baselet.gwt.client.view.VersionChecker;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
@@ -16,16 +18,26 @@ public class DownloadPopupPanel extends MyPopupPanel {
 
 	private static DateTimeFormat DTF = DateTimeFormat.getFormat("yyyy-MM-dd HH-mm-ss");
 
-	public DownloadPopupPanel(final String uxfUrl, final String pngUrl, final FilenameHolder filenameHolder) {
+	public DownloadPopupPanel(final String uxfUrl, final String pngUrl, Diagram diagram, final FilenameAndScaleHolder filenameAndScaleHolder) {
 		super(true, Type.POPUP);
 		if (VersionChecker.GetVersion() == VersionChecker.Version.VSCODE) {
 			setHeader("Export Diagram");
 			FlowPanel panel = new FlowPanel();
-			HTML w = new HTML("Optionally set a filename");
-			panel.add(w);
 			// final TextBox textBox = new TextBox();
 			// panel.add(textBox);
-			// textBox.setValue(filenameHolder.getFilename());
+			// textBox.setValue(filenameAndScaleHolder.getFilename());
+
+
+
+			//handle scaling
+			HTML scaleHtml = new HTML("Set scaling of Image file:");
+			panel.add(scaleHtml);
+			final TextBox scaleBox = new TextBox();
+			panel.add(scaleBox);
+			scaleBox.setValue("1.0");
+			filenameAndScaleHolder.setScaling(1d);
+
+			panel.add(new HTML("<br>"));
 			Button saveDiagramButton = new Button();
 			saveDiagramButton.setText("Save Diagram File");
 			saveDiagramButton.addClickHandler(new ClickHandler() {
@@ -39,15 +51,39 @@ public class DownloadPopupPanel extends MyPopupPanel {
 			savePictureButton.addClickHandler(new ClickHandler() {
 				@Override
 				public void onClick(ClickEvent event) {
-					exportPngVSCode(pngUrl);
+					String scaledPngUrl = CanvasUtils.createPngCanvasDataUrl(diagram, filenameAndScaleHolder.getScaling());
+					exportPngVSCode(scaledPngUrl);
+
+					/*
+						this is line is the prevent a bug in the vs code version
+						if this is not recalculated with a 1.0 scaling, then the display size of the elements in the diagram will change
+						to whatever scaling was calculated for exporting
+					 */
+					CanvasUtils.createPngCanvasDataUrl(diagram, 1.0d);
 				}
 			});
 			savePictureButton.setText("Save Image File");
 			panel.add(savePictureButton);
 			setWidget(panel);
+			//renew download links when scaling is changed
+			scaleBox.addDomHandler(new InputHandler() {
+				@Override
+				public void onInput(InputEvent event) {
+					try {
+						double scalingValue = Double.parseDouble(scaleBox.getValue());
+						if (scalingValue <= 0)
+							throw new Exception();
+						filenameAndScaleHolder.setScaling(scalingValue);
+					} catch (Exception e)
+					{
+						//wrong scaling value, just default to standard
+						filenameAndScaleHolder.setScaling(1.0d);
+					}
+				}
+			}, InputEvent.getType());
 			// listen to all input events from the browser (http://stackoverflow.com/a/43089693)
 			/* textBox.addDomHandler(new InputHandler() {
-			 * @Override public void onInput(InputEvent event) { //not needed in vs code version since filename will be entered in popup dialog //filenameHolder.setFilename(textBox.getText()); } }, InputEvent.getType()); */
+			 * @Override public void onInput(InputEvent event) { //not needed in vs code version since filename will be entered in popup dialog //filenameAndScaleHolder.setFilename(textBox.getText()); } }, InputEvent.getType()); */
 		}
 		else {
 			setHeader("Export Diagram");
@@ -56,8 +92,17 @@ public class DownloadPopupPanel extends MyPopupPanel {
 			panel.add(w);
 			final TextBox textBox = new TextBox();
 			panel.add(textBox);
-			textBox.setValue(filenameHolder.getFilename());
-			final HTML downloadLinkHtml = new HTML(createDownloadLinks(uxfUrl, pngUrl, filenameHolder.getFilename()));
+
+			//handle scaling
+			HTML scaleHtml = new HTML("Set scaling of Image file:");
+			panel.add(scaleHtml);
+			final TextBox scaleBox = new TextBox();
+			panel.add(scaleBox);
+			scaleBox.setValue("1.0");
+			filenameAndScaleHolder.setScaling(1d);
+
+			textBox.setValue(filenameAndScaleHolder.getFilename());
+			final HTML downloadLinkHtml = new HTML(createDownloadLinks(uxfUrl, pngUrl, filenameAndScaleHolder.getFilename()));
 			panel.add(downloadLinkHtml);
 			panel.add(new HTML("<div style=\"color:gray;\">To change the target directory</div><div style=\"color:gray;\">use \"Right click -&gt; Save as\"</div>"));
 			setWidget(panel);
@@ -65,10 +110,32 @@ public class DownloadPopupPanel extends MyPopupPanel {
 			textBox.addDomHandler(new InputHandler() {
 				@Override
 				public void onInput(InputEvent event) {
-					filenameHolder.setFilename(textBox.getText());
-					downloadLinkHtml.setHTML(createDownloadLinks(uxfUrl, pngUrl, filenameHolder.getFilename()));
+					filenameAndScaleHolder.setFilename(textBox.getText());
+					String scaledPngUrl = CanvasUtils.createPngCanvasDataUrl(diagram, filenameAndScaleHolder.getScaling());
+					downloadLinkHtml.setHTML(createDownloadLinks(uxfUrl, scaledPngUrl, filenameAndScaleHolder.getFilename()));
 				}
 			}, InputEvent.getType());
+			//renew download links when scaling is changed
+			scaleBox.addDomHandler(new InputHandler() {
+				@Override
+				public void onInput(InputEvent event) {
+					try {
+						double scalingValue = Double.parseDouble(scaleBox.getValue());
+						if (scalingValue <= 0)
+							throw new Exception();
+						filenameAndScaleHolder.setScaling(scalingValue);
+						String scaledPngUrl = CanvasUtils.createPngCanvasDataUrl(diagram, filenameAndScaleHolder.getScaling());
+						downloadLinkHtml.setHTML(createDownloadLinks(uxfUrl, scaledPngUrl, filenameAndScaleHolder.getFilename()));
+					} catch (Exception e)
+					{
+						//wrong scaling value, just default to standard
+						filenameAndScaleHolder.setFilename(textBox.getText());
+						filenameAndScaleHolder.setScaling(1.0d);
+						downloadLinkHtml.setHTML(createDownloadLinks(uxfUrl, pngUrl, filenameAndScaleHolder.getFilename()));
+					}
+				}
+			}, InputEvent.getType());
+			//String pngUrl = CanvasUtils.createPngCanvasDataUrl(diagramPanel.getDiagram());
 		}
 	}
 
