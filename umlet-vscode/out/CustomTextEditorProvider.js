97f1ca27f7265331988e40424b0e7e51a0c0ba6f
T B <e01306821@student.tuwien.ac.at>
2020-06-26 14:19:35 +0200

    UVC-010: Converted umlet-vscode to a GWT module

--- umlet-vscode/out/CustomTextEditorProvider.js
+++ umlet-vscode/out/CustomTextEditorProvider.js
@@ -65,7 +65,7 @@ class UmletEditorProvider {
         });
         webviewPanel.webview.options = {
             enableScripts: true,
-            localResourceRoots: [vscode.Uri.file(path.join(this.context.extensionPath, 'src', 'umlet-gwt'))]
+            localResourceRoots: [vscode.Uri.file(path.join(this.context.extensionPath, 'target', 'umlet-vscode-14.4.0-SNAPSHOT', 'umletvscode'))]
         };
         let WebviewPanelOptions = webviewPanel.options;
         WebviewPanelOptions = {
@@ -103,7 +103,7 @@ class UmletEditorProvider {
             }
         }, undefined, this.context.subscriptions);
         // Get path to resource on disk
-        const onDiskPath = vscode.Uri.file(path.join(this.context.extensionPath, 'src', 'umlet-gwt'));
+        const onDiskPath = vscode.Uri.file(path.join(this.context.extensionPath, 'target', 'umlet-vscode-14.4.0-SNAPSHOT', 'umletvscode'));
         // And get the special URI to use with the webview
         const localUmletFolder = webviewPanel.webview.asWebviewUri(onDiskPath);
         let fileContents = document.getText().toString();
@@ -203,7 +203,7 @@ class UmletEditorProvider {
       <link type="text/css" rel="stylesheet" href="umletino.css">
       <link rel="icon" type="image/x-icon" href="favicon.ico">
       <title>UMLetino - Free Online UML Tool for Fast UML Diagrams</title>
-      <script type="text/javascript" src="baseletgwt/baseletgwt.nocache.js?2020-03-15_09-48-08"></script>
+      <script type="text/javascript" src="umletvscode/umletvscode.nocache.js?2020-03-15_09-48-08"></script>
     </head>
     <body>
       <!-- the following line is necessary for history support -->
