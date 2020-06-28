065644610f6bad4608d576448dd62a53b6fd2e4a
Tekbert <megabite@web.de>
2020-06-28 13:23:14 +0200

    UVC-900+3: workarround for opening new file in umletino

--- umlet-vscode/src/extension.ts
+++ umlet-vscode/src/extension.ts
@@ -54,6 +54,45 @@ function createUmletCommands(context: vscode.ExtensionContext) {
   };
   context.subscriptions.push(vscode.commands.registerCommand('umlet.exportPngFour', commandHandlerFourExport));
 
+  //create a new file and open it in editor
+  const commandHandlerCreateNewDiagram = () => {
+    let folderPath = vscode.workspace.rootPath;
+    if (folderPath === undefined)
+    {
+      vscode.window.showErrorMessage("Unable to create new .uxf file, since there is currently no folder/workspace opened. Please open a folder/workspace and try again!");
+      return;
+    }
+    let fileName = "Diagram "+ getCurrentDateTimeString() + ".uxf"; 
+    
+    let completePath = path.join(folderPath , fileName);
+    
+    fs.writeFile(completePath, "", function (err) {
+      if (err) { return console.log(err); }
+    });
+    let url = Uri.file(completePath);
+    /*
+      note: this does not guarantee the new file is opened with UMLet, but currently there does not seem to be an option to tell vscode to open a file with a certain editor.
+      VSCode prefers Custom Editors over its built-in editors, but especially if the extention is not fully loaded yet, vscode might open this file as plaintext. 
+      To prevent this case "onStartupFinished" is a startup event for umlet, so the probability that the umlet extension is not loaded when a user calls the command is greatly reduced.
+      if other .uxf extensions are installed, vscode might not open the newly created file with UMLet as well, and has to be manually reopened in UMLet
+    */
+    vscode.commands.executeCommand('vscode.open', url); 
+  };
+  context.subscriptions.push(vscode.commands.registerCommand('umlet.createNewDiagram', commandHandlerCreateNewDiagram));
+
+  function getCurrentDateTimeString(): string {
+    let today = new Date();
+    let dd = String(today.getDate()).padStart(2, '0');
+    let mm = String(today.getMonth() + 1).padStart(2, '0'); 
+    let yyyy = today.getFullYear();
+
+    let hh = String( today.getHours()).padStart(2, '0');
+    let mins = String(today.getMinutes()).padStart(2, '0');
+    let ss = String(today.getSeconds()).padStart(2, '0');
+
+    return ("" + yyyy + "-" + mm + "-" + dd + " " + hh + "-" + mins + "-" + ss );
+  }
+
   //keyboard commands for copy paste cut
   //copy
   const commandHandlerKeyboardCopy = () => {
