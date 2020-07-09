e9c81bbce8046e21acde4903e4db116108c2d2d0
T B <e01306821@student.tuwien.ac.at>
2020-07-09 13:03:03 +0200

    Letting UMLet take care of file change detection again

--- umlet-web/src/main/java/com/web/gwt/client/file/WebFileChangeNotifier.java
+++ umlet-web/src/main/java/com/web/gwt/client/file/WebFileChangeNotifier.java
@@ -1,11 +1,10 @@
 package com.web.gwt.client.file;
 
-import com.baselet.element.interfaces.Diagram;
 import com.baselet.gwt.client.file.FileChangeNotifier;
 
 public class WebFileChangeNotifier implements FileChangeNotifier {
     @Override
-    public void notifyFileChange(Diagram diagram) {
+    public void notifyFileChange(String diagramXMLState) {
         // Web does not support this feature
     }
 }
