e98cfe1a563daa27d0154df2a1c3b65d890ad70e
T B <e01306821@student.tuwien.ac.at>
2020-07-09 14:34:01 +0200

    Removed now obsolete class

--- umlet-vscode/src/main/java/com/vscode/gwt/client/view/utils/VSCodeStartupDiagramLoader.java
+++ umlet-vscode/src/main/java/com/vscode/gwt/client/view/utils/VSCodeStartupDiagramLoader.java
@@ -1,9 +1,6 @@
 package com.vscode.gwt.client.view.utils;
 
-import com.baselet.gwt.client.element.DiagramXmlParser;
-import com.baselet.gwt.client.view.VersionChecker;
 import com.baselet.gwt.client.view.utils.StartupDiagramLoader;
-import com.google.gwt.core.client.GWT;
 
 public class VSCodeStartupDiagramLoader implements StartupDiagramLoader {
     @Override
