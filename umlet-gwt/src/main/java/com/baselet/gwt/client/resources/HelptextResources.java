6d71d386d8c2dc9cf9fa22b33ab7ee82767d4503
T B <e01306821@student.tuwien.ac.at>
2020-07-17 13:42:42 +0200

    UVC: Showing custom helptext on empty diagram view in VSCode

---
+++ umlet-gwt/src/main/java/com/baselet/gwt/client/resources/HelptextResources.java
@@ -0,0 +1,9 @@
+package com.baselet.gwt.client.resources;
+
+import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.TextResource;
+
+public interface HelptextResources extends ClientBundle {
+    @Source("Helptext.txt")
+    TextResource helpText();
+}
---
+++ umlet-vscode/src/main/java/com/vscode/gwt/client/resources/VsCodeHelptextResources.java
@@ -0,0 +1,9 @@
+package com.vscode.gwt.client.resources;
+
+import com.baselet.gwt.client.resources.HelptextResources;
+import com.google.gwt.resources.client.TextResource;
+
+public interface VsCodeHelptextResources extends HelptextResources {
+    @Source("Helptext.txt")
+    TextResource helpText();
+}
