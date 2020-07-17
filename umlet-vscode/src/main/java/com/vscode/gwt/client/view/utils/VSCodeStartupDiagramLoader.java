09e266ba76d9d7da6ebc56c31a510baef1876787
T B <e01306821@student.tuwien.ac.at>
2020-07-17 12:21:29 +0200

    UVC: Integrated diagram loader of web version into new modularisation

--- umlet-vscode/src/main/java/com/vscode/gwt/client/view/utils/VSCodeStartupDiagramLoader.java
+++ umlet-vscode/src/main/java/com/vscode/gwt/client/view/utils/VSCodeStartupDiagramLoader.java
@@ -1,12 +1,38 @@
 package com.vscode.gwt.client.view.utils;
 
+import com.baselet.gwt.client.element.DiagramXmlParser;
+import com.baselet.gwt.client.logging.CustomLogger;
+import com.baselet.gwt.client.logging.CustomLoggerFactory;
+import com.baselet.gwt.client.view.DrawPanel;
+import com.baselet.gwt.client.view.DrawPanelDiagram;
+import com.baselet.gwt.client.view.MainView;
 import com.baselet.gwt.client.view.utils.StartupDiagramLoader;
 
 public class VSCodeStartupDiagramLoader implements StartupDiagramLoader {
+
+    private CustomLogger log = CustomLoggerFactory.getLogger(VSCodeStartupDiagramLoader.class);
+
     @Override
-    public String loadDiagram() {
+    public void loadDiagram(MainView mainView) {
         //Retrieve the Diagram
-        return getVsCodePredefinedFile();
+        String uxf = getVsCodePredefinedFile();
+
+        // In case a plain, newly created empty file was loaded, UMLet will create the default empty workspace
+        // if its not empty, it will load it
+        if (uxf != null && !uxf.equals("")) {
+            try {
+                mainView.setDiagram(DiagramXmlParser.xmlToDiagram(uxf));
+            } catch (Exception e) {
+                log.error("failed to load diagram passed from startup, loading defaults...");
+            }
+        } else {
+            DrawPanel drawPanel = mainView.getDiagramPanel();
+            log.info("current diag is at start:" + DiagramXmlParser.diagramToXml(drawPanel.getDiagram()));
+            //if diagram is indeed empty, a new empty diagram file was opened
+            //send the 'basic' empty umlet file data back to vscode
+            if (mainView.getDiagramPanel() instanceof DrawPanelDiagram)
+                ((DrawPanelDiagram) drawPanel).handleFileUpdate();
+        }
     }
 
     private static native String getVsCodePredefinedFile() /*-{
