242c35f25c0c2ff144773829818594bab78d0911
911992 <45896677+911992@users.noreply.github.com>
2020-11-18 13:26:27 -0800

    14.4.0-SNAPSHOT_string_type_for_group_attribute(20201118)_r0
    Majors:
    • Allowing String literals for element's group attribute

--- umlet-elements/src/main/java/com/baselet/element/facet/common/GroupFacet.java
+++ umlet-elements/src/main/java/com/baselet/element/facet/common/GroupFacet.java
@@ -3,26 +3,52 @@ package com.baselet.element.facet.common;
 import com.baselet.diagram.draw.helper.StyleException;
 import com.baselet.element.facet.FirstRunKeyValueFacet;
 import com.baselet.element.facet.PropertiesParserState;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class GroupFacet extends FirstRunKeyValueFacet {
 
-	public static final GroupFacet INSTANCE = new GroupFacet();
+   public static final GroupFacet INSTANCE = new GroupFacet();
 
-	private GroupFacet() {}
+    private GroupFacet() {}
 
-	public static final String KEY = "group";
+    public static final String KEY = "group";
+    public static final String VALUE_PREFIX = "group";
+    private static final int KEY_START_VAL = 0;
+    public static final AtomicInteger LAST_KNOWN_VAl = new AtomicInteger(KEY_START_VAL);
 
-	@Override
-	public KeyValue getKeyValue() {
-		return new KeyValue(KEY, false, "1", "grouped elements are selected at once");
-	}
+    public static final String nextDefaultKey() {
+        return String.format("%s-%d", VALUE_PREFIX, LAST_KNOWN_VAl.getAndIncrement());
+    }
 
-	@Override
-	public void handleValue(String value, PropertiesParserState state) {
-		try {
-			state.setFacetResponse(GroupFacet.class, Integer.valueOf(value));
-		} catch (NumberFormatException e) {
-			throw new StyleException("value must be a positive or negative integer");
-		}
-	}
+    @Override
+    public KeyValue getKeyValue() {
+        return new KeyValue(KEY, false, String.format("%s-%d", VALUE_PREFIX, KEY_START_VAL), "grouped elements are selected at once");
+    }
+
+    /**
+     * trimming the group value, returns null when val is not valid
+     */
+    public static String getElementGroupValSafe(String arg_group) {
+        if (arg_group == null || arg_group.length()==0) {
+            return null;
+        }
+        String _res = arg_group.trim();
+        if (_res.length() == 0) {
+            return null;
+        }
+        return _res;
+    }
+
+    @Override
+    public void handleValue(String value, PropertiesParserState state) {
+        String safeValue = getElementGroupValSafe(value);
+        if(safeValue==null || !value.equals(safeValue)){
+            if(value.length()>0){
+                throw new StyleException("Group value cannot have lead-tail whitespaces");
+            }else{
+                throw new StyleException("Group value cannot be empty");
+            }
+        }
+        state.setFacetResponse(GroupFacet.class, value);
+    }
 }
