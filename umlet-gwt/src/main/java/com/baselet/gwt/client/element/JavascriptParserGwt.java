69f269b7149b8cee9ad3e575b10523f9ea8ac7e1
Georgi Zhelyazkov <georgi.zhv@gmail.com>
2021-04-02 16:17:02 +0200

    * format code

--- umlet-gwt/src/main/java/com/baselet/gwt/client/element/JavascriptParserGwt.java
+++ umlet-gwt/src/main/java/com/baselet/gwt/client/element/JavascriptParserGwt.java
@@ -7,6 +7,7 @@ import com.baselet.gwt.client.logging.CustomLogger;
 import com.baselet.gwt.client.logging.CustomLoggerFactory;
 
 public class JavascriptParserGwt extends JavascriptCodeParser {
+
 	private static final CustomLogger log = CustomLoggerFactory.getLogger(JavascriptParserGwt.class);
 
 	private final DrawHandler drawer;
@@ -50,31 +51,31 @@ public class JavascriptParserGwt extends JavascriptCodeParser {
 																																			}-*/;
 
 	public native void exportDrawFunctions(DrawHandler drawer, int width, int height) /*-{
-																													$wnd.width = width;
-																													$wnd.height = height;
+																						$wnd.width = width;
+																						$wnd.height = height;
 
-																													$wnd.drawCircle = $entry(function(x,y,radius) {
-																													drawer.@com.baselet.gwt.client.element.DrawHandlerGwt::drawCircle(DDD)(x,y,radius);
-																													});
-																													$wnd.drawRectangle = $entry(function(x,y,width,height) {
-																													drawer.@com.baselet.gwt.client.element.DrawHandlerGwt::drawRectangle(DDDD)(x,y,width,height);
-																													});
-																													$wnd.drawLine = $entry(function(x1,y1,x2,y2) {
-																													drawer.@com.baselet.gwt.client.element.DrawHandlerGwt::drawLine(DDDD)(x1,y1,x2,y2);
-																													});
-																													$wnd.drawRectangleRound = $entry(function(x,y,width,height,radius) {
-																													drawer.@com.baselet.gwt.client.element.DrawHandlerGwt::drawRectangleRound(DDDDD)(x,y,width,height,radius);
-																													});
-																													$wnd.drawEllipse = $entry(function(x,y,width,height) {
-																													drawer.@com.baselet.gwt.client.element.DrawHandlerGwt::drawEllipse(DDDD)(x,y,width,height);
-																													});
-																													$wnd.drawArc = $entry(function(x,y,width,height,start,extent,open) {
-																													drawer.@com.baselet.gwt.client.element.DrawHandlerGwt::drawArc(DDDDDDZ)(x,y,width,height,start,extent,open);
-																													});
-																													$wnd.drawText = $entry(function(multiLineWithMarkup,x,y,align) {
-																													drawer.@com.baselet.gwt.client.element.DrawHandlerGwt::print(Ljava/lang/String;DDLcom/baselet/control/enums/AlignHorizontal;)(multiLineWithMarkup,x,y,align);
-																													});
+																						$wnd.drawCircle = $entry(function(x,y,radius) {
+																						drawer.@com.baselet.gwt.client.element.DrawHandlerGwt::drawCircle(DDD)(x,y,radius);
+																						});
+																						$wnd.drawRectangle = $entry(function(x,y,width,height) {
+																						drawer.@com.baselet.gwt.client.element.DrawHandlerGwt::drawRectangle(DDDD)(x,y,width,height);
+																						});
+																						$wnd.drawLine = $entry(function(x1,y1,x2,y2) {
+																						drawer.@com.baselet.gwt.client.element.DrawHandlerGwt::drawLine(DDDD)(x1,y1,x2,y2);
+																						});
+																						$wnd.drawRectangleRound = $entry(function(x,y,width,height,radius) {
+																						drawer.@com.baselet.gwt.client.element.DrawHandlerGwt::drawRectangleRound(DDDDD)(x,y,width,height,radius);
+																						});
+																						$wnd.drawEllipse = $entry(function(x,y,width,height) {
+																						drawer.@com.baselet.gwt.client.element.DrawHandlerGwt::drawEllipse(DDDD)(x,y,width,height);
+																						});
+																						$wnd.drawArc = $entry(function(x,y,width,height,start,extent,open) {
+																						drawer.@com.baselet.gwt.client.element.DrawHandlerGwt::drawArc(DDDDDDZ)(x,y,width,height,start,extent,open);
+																						});
+																						$wnd.drawText = $entry(function(multiLineWithMarkup,x,y,align) {
+																						drawer.@com.baselet.gwt.client.element.DrawHandlerGwt::print(Ljava/lang/String;DDLcom/baselet/control/enums/AlignHorizontal;)(multiLineWithMarkup,x,y,align);
+																						});
 
-																													}-*/;
+																						}-*/;
 
 }
