ac2a476bc9bb0bdebb50add4b6cdc36a73eb65cd
Andreas <afdia@users.noreply.github.com>
2022-04-11 17:29:34 +0200

    #673: improved browser selection for linux

--- umlet-swing/src/main/java/com/baselet/gui/BrowserLauncher.java
+++ umlet-swing/src/main/java/com/baselet/gui/BrowserLauncher.java
@@ -22,34 +22,43 @@ public class BrowserLauncher {
 		try {
 			// Since Java6 this is a much easier method to open the browser
 			if (Desktop.isDesktopSupported()) {
-				Desktop desktop = Desktop.getDesktop();
-				desktop.browse(new URI(url));
+				try {
+					Desktop desktop = Desktop.getDesktop();
+					desktop.browse(new URI(url));
+					return;
+				} catch (Exception e) {
+					// #673: it is possible to have isDesktopSupported() return true but browser() still fails; then continue and try alternative browser selection
+				}
 			}
 
-			// Only if desktop is not supported we try the old main specific code
-			else {
-				if (SystemInfo.OS == Os.MAC) {
-					Class<?> fileMgr = Class.forName("com.apple.eio.FileManager");
-					Method openURL = fileMgr.getDeclaredMethod("openURL", new Class[] { String.class });
-					openURL.invoke(null, new Object[] { url });
+			// If desktop is not supported we try the old main specific code
+			if (SystemInfo.OS == Os.MAC) {
+				Class<?> fileMgr = Class.forName("com.apple.eio.FileManager");
+				Method openURL = fileMgr.getDeclaredMethod("openURL", new Class[] { String.class });
+				openURL.invoke(null, new Object[] { url });
+			}
+			else if (SystemInfo.OS == Os.WINDOWS) {
+				Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler " + url);
+			}
+			else { // assume Unix or Linux
+				try {
+					Runtime.getRuntime().exec(new String[] { "xdg-open", url }); // #673 try xdg-open (see https://stackoverflow.com/questions/70348991/is-desktopbrowse-supported-on-linux-platform-just-for-gnome-desktop/70434441#70434441)
+					return;
+				} catch (IOException e) {
+					// if it fails continue trying different browser names
 				}
-				else if (SystemInfo.OS == Os.WINDOWS) {
-					Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler " + url);
+				String[] browsers = { "firefox", "chromium", "opera", "konqueror", "epiphany", "mozilla", "netscape" };
+				String browser = null;
+				for (int count = 0; count < browsers.length && browser == null; count++) {
+					if (Runtime.getRuntime().exec(new String[] { "which", browsers[count] }).waitFor() == 0) {
+						browser = browsers[count];
+					}
 				}
-				else { // assume Unix or Linux
-					String[] browsers = { "firefox", "opera", "konqueror", "epiphany", "mozilla", "netscape" };
-					String browser = null;
-					for (int count = 0; count < browsers.length && browser == null; count++) {
-						if (Runtime.getRuntime().exec(new String[] { "which", browsers[count] }).waitFor() == 0) {
-							browser = browsers[count];
-						}
-					}
-					if (browser == null) {
-						throw new Exception("Could not find web browser");
-					}
-					else {
-						Runtime.getRuntime().exec(new String[] { browser, url });
-					}
+				if (browser == null) {
+					throw new Exception("Could not find web browser");
+				}
+				else {
+					Runtime.getRuntime().exec(new String[] { browser, url });
 				}
 			}
 		} catch (Exception e) {
